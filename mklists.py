e554f0a53d98579b43ff0797b23d3d7b1946b562
Tom Baker <tom@tombaker.org>
2017-06-07 09:53:46 +0200

    mklists.py

--- mklists.py
+++ mklists.py
@@ -4,4 +4,13 @@ def md2html(text_string):
     URL_REGEX = re.compile(r'''((?:mailto:|ftp://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
     return URL_REGEX.sub(r'<a href="\1">\1</a>', text_string)
 
+class ListLine():
+
+    def __init__(self, line_of_list):
+        self.line = line_of_list
+
+    def urlify(self):
+        URL_REGEX = re.compile(r'''((?:mailto:|ftp://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
+        return URL_REGEX.sub(r'<a href="\1">\1</a>', self.line)
+
 
