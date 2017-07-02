305baab244d15457c3a350df5282d245079e14c0
Tom Baker <tom@tombaker.org>
2017-07-02 21:09:41 +0200

    ftp:// => git://

--- mklists.py
+++ mklists.py
@@ -1,7 +1,7 @@
 import re
 
 def md2html(text_string):
-    URL_REGEX = re.compile(r'''((?:mailto:|ftp://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
+    URL_REGEX = re.compile(r'''((?:mailto:|git://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
     return URL_REGEX.sub(r'<a href="\1">\1</a>', text_string)
 
 class ListLine():
@@ -10,7 +10,7 @@ class ListLine():
         self.line = line_of_list
 
     def urlify(self):
-        URL_REGEX = re.compile(r'''((?:mailto:|ftp://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
+        URL_REGEX = re.compile(r'''((?:mailto:|git://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
         return URL_REGEX.sub(r'<a href="\1">\1</a>', self.line)
 
 
