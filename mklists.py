ec135c2369dff7a8b995ef892709738b47950310
Tom Baker <tom@tombaker.org>
2017-07-18 10:26:10 +0200

    still passes after making URL_REGEX a module-level constant

--- mklists.py
+++ mklists.py
@@ -1,5 +1,7 @@
 import re
 
+URL_REGEX = re.compile(r"""((?:mailto:|git://|http://|https://)[^ <>'"{},|\\^`[\]]*)""")
+
 def md2html(text_string):
     URL_REGEX = re.compile(r'''((?:mailto:|git://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
     return URL_REGEX.sub(r'<a href="\1">\1</a>', text_string)
@@ -12,7 +14,7 @@ class ListLine():
     def linkified_line(self):
         """
         """
-        URL_REGEX = re.compile(r'''((?:mailto:|git://|http://|https://)[^ <>'"{},|\\^`[\]]*)''')
+        FILE_REGEX = re.compile(r"""((/Users/tbaker|/home/tbaker)[^ <>'"{},|\\^`[\]]*)""")
         if '<a href=' in self.line:
             return self.line
         return URL_REGEX.sub(r'<a href="\1">\1</a>', self.line)
