57a95ac7d1e373e4e0eb50f4e5f05e568b6db040
Tom Baker <tom@tombaker.org>
2017-07-18 11:10:45 +0200

    delete addbr(), move URL_REGEX back into method linkified_line() - tests still pass

--- mklists.py
+++ mklists.py
@@ -1,12 +1,8 @@
 import re
 
-URL_REGEX = re.compile(r"""((?:mailto:|git://|http://|https://)[^ <>'"{},|\\^`[\]]*)""")
-
-def md2html(text_string):
-    URL_REGEX = re.compile(r'''((?:mailto:|git://|http://|https://)[^ <>'"{}|\\^`[\]]*)''')
-    return URL_REGEX.sub(r'<a href="\1">\1</a>', text_string)
 
 class ListLine():
+    """@@@"""
 
     def __init__(self, line_of_list):
         self.line = line_of_list
@@ -14,7 +10,7 @@ class ListLine():
     def linkified_line(self):
         """
         """
-        FILE_REGEX = re.compile(r"""((/Users/tbaker|/home/tbaker)[^ <>'"{},|\\^`[\]]*)""")
+        URL_REGEX = re.compile(r"""((?:mailto:|git://|http://|https://)[^ <>'"{},|\\^`[\]]*)""")
         if '<a href=' in self.line:
             return self.line
         return URL_REGEX.sub(r'<a href="\1">\1</a>', self.line)
