94f379f6635d5dbbe8ccea375166aea7d137130c
Tom Baker <tom@tombaker.org>
2017-09-08 16:15:28 +0200

    cleaning up, removing print statements

---
+++ get_globlines.py
@@ -0,0 +1,9 @@
+def get_globlines_list(cwd=os.getcwd()):
+    """Something like:
+    globlines_list = []
+    for file in glob.glob('*'):
+        globlines_list.append(file.readlines())
+    return globlines_list
+    """
+    pass
+
--- new_shuffle.py
+++ new_shuffle.py
@@ -3,65 +3,35 @@ import glob
 import os
 import re
 
-def get_globlines_list(cwd=os.getcwd()):
-    """Something like:
-    globlines_list = []
-    for file in glob.glob('*'):
-        globlines_list.append(file.readlines())
-    return globlines_list
-    """
-    pass
-
 def shuffle(rules_list, globlines_list):
-    """
-    Rule = namedtuple('Rule', 'sourcematch_awkfield source_matchregex source target targetsort_awkfield')
-
-    sourcematch_awkfield = 0 should mean "anywhere in the (unsplit) line, as in awk"
-
-    Expected rules should look something like this:
-    [
-        Rule(sourcematch_awkfield='a', source_matchregex='b', source='c', target='d', targetsort_awkfield='e'), 
-        Rule(sourcematch_awkfield='f', source_matchregex='g', source='h', target='i', targetsort_awkfield='j')
-    ]
-    """
+    """Rule = namedtuple('Rule', 'sourcematch_awkfield source_matchregex source target targetsort_awkfield')"""
 
     # Initialize dictionary mklists_dict (where everything happens) with:
     # -- key: source 
     # -- value: list of lines (previously aggregated from all text files in the working directory)
     mklists_dict = defaultdict(list)
     mklists_dict[rules_list[0].source] = globlines_list
-    ### print('mklists_dict is {}'.format(mklists_dict))
 
     for rule in rules_list:
-        print(rule)
-        ### print('mklists_dict[rule.source] is {}'.format(mklists_dict[rule.source]))
         for line in mklists_dict[rule.source]:
-            awkfield = rule.sourcematch_awkfield
-            ethfield = rule.sourcematch_awkfield - 1
-            regex    = rule.source_matchregex
-            target   = rule.target
-            source   = rule.source
-            ### print('awkfield is {}'.format(awkfield))
-            ### print('ethfield is {}'.format(ethfield))
+            awkfield    = rule.sourcematch_awkfield
+            ethfield    = rule.sourcematch_awkfield - 1
+            regex       = rule.source_matchregex
+            target      = rule.target
+            source      = rule.source
+            eth_sortkey = rule.targetsort_awkfield - 1
 
             # if awkfield out of range, then source and target remain unchanged
             if awkfield > len(line.split()):
-                ### print('line is {}'.format(line))
-                ### print('length of line is {}'.format(len(line.split())))
-                ### print('awkfield is greater than field length of line')
                 continue                       
 
-            # if awkfield is zero, then regex matched to entire line
+            # if awkfield is zero, then regex matched against entire line
             if awkfield == 0:                
-                ### print('awkfield is zero')
                 mklists_dict[target].extend([line for line in mklists_dict[source] if re.search(regex, line)])
                 mklists_dict[source] = [line for line in mklists_dict[source] if not re.search(regex, line)]
 
-            # if awkfield is greater than zero (and within range), then regex matched to specific field
+            # if awkfield is greater than zero (and within range), then regex matched against specific field
             if awkfield > 0:
-                ### print('awkfield is greater than zero')
-                ### print('mklists_dict is {}'.format(mklists_dict))
-                ### print('mklists_dict[target] is {}'.format(mklists_dict[target])
                 mklists_dict[target].extend([line for line in mklists_dict[source] if re.search(regex, line.split()[ethfield])])
                 mklists_dict[source] = [line for line in mklists_dict[source] if not re.search(regex, line.split()[ethfield])]
         
@@ -76,20 +46,6 @@ if __name__ == "__main__":
     print(x)
 
     """
-    for rule in rules:
-        mklists_dict[source] = [line for line in lines if not re.search(searchkey, line)] # over-writes
-        mklists_dict[target].append([line for line in lines if re.search(searchkey, line)]) # appends
         if sort_order > 0:
             mklists_dict[target] according to targetsort_awkfield
-
-    So why does this use append() - we want extend(), right?
-    >>> x = [1, 2, 3, 4]
-    >>> x.extend([5, 6, 7, 8])
-    >>> x
-    [1, 2, 3, 4, 5, 6, 7, 8]
-
-    >>> x = [1, 2, 3, 4]
-    >>> x.append([5, 6, 7, 8])
-    >>> x
-    [1, 2, 3, 4, [5, 6, 7, 8]]
     """
