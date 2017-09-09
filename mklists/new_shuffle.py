26aa2c7bf74271351457ec8fca738fa5dd4f01ac
Tom Baker <tom@tombaker.org>
2017-09-09 09:58:53 +0200

    shorten variable names to fit line length 99 as per PEP8

--- mklists/new_shuffle.py
+++ mklists/new_shuffle.py
@@ -3,55 +3,45 @@ import glob
 import os
 import re
 
-def shuffle(rules_list, globlines_list):
-    """Rule = namedtuple('Rule', 'sourcematch_awkfield source_matchregex source target targetsort_awkfield')"""
+def shuffle(rules_l, globlines_l):
+    """Rule = namedtuple('Rule', 'srcmatch_awkf srcmatch_rgx src trg trgsort_awkf')"""
 
-    # Initialize dictionary mklists_dict (where everything happens) with:
-    # -- key: source 
-    # -- value: list of lines (previously aggregated from all text files in the working directory)
-    mklists_dict = defaultdict(list)
-    mklists_dict[rules_list[0].source] = globlines_list
+    # Initialize dictionary mkl_d (where everything happens) with:
+    # -- key: rule.source
+    # -- value: globlines_l, an aggregated list of lines from all text files in working directory
+    mkl_d = defaultdict(list)
+    mkl_d[rules_l[0].src] = globlines_l
 
-    for rule in rules_list:
-        for line in mklists_dict[rule.source]:
-            awkfield    = rule.sourcematch_awkfield
-            ethfield    = rule.sourcematch_awkfield - 1
-            regex       = rule.source_matchregex
-            target      = rule.target
-            source      = rule.source
+    for rule in rules_l:
+        for ln in mkl_d[rule.src]:
+            awkf    = rule.srcmatch_awkf
+            ethf    = rule.srcmatch_awkf - 1
+            rgx     = rule.srcmatch_rgx
+            trg     = rule.trg
+            src     = rule.src
 
-            # if awkfield out of range, then source and target remain unchanged
-            if awkfield > len(line.split()):
+            # if awkf out of range, then src and trg remain unchanged
+            # in srcmatch_awkf, zero means "match against entire line"
+            if awkf > len(ln.split()):
                 continue                       
 
-            # if awkfield is zero, then regex matched against entire line
-            if awkfield == 0:                
-                mklists_dict[target].extend([line for line in mklists_dict[source] if re.search(regex, line)])
-                mklists_dict[source] = [line for line in mklists_dict[source] if not re.search(regex, line)]
+            # if awkf is zero, then regex matched against entire line
+            if awkf == 0:                
+                mkl_d[trg].extend([ln for ln in mkl_d[src] if re.search(rgx, ln)])
+                mkl_d[src] = [ln for ln in mkl_d[src] if not re.search(rgx, ln)]
 
-            # if awkfield is greater than zero (and within range), then regex matched against specific field
-            if awkfield > 0:
-                mklists_dict[target].extend([line for line in mklists_dict[source] if re.search(regex, line.split()[ethfield])])
-                mklists_dict[source] = [line for line in mklists_dict[source] if not re.search(regex, line.split()[ethfield])]
+            # if awkf greater than zero (and within range), regex matched against specific field
+            if awkf > 0:
+                mkl_d[trg].extend([ln for ln in mkl_d[src] if re.search(rgx, ln.split()[ethf])])
+                mkl_d[src] = [ln for ln in mkl_d[src] if not re.search(rgx, ln.split()[ethf])]
 
-            # sort mklists_dict[target], dsu-style, if targetsort_awkfield is greater than zero (note: zero is false)
-            if rule.targetsort_awkfield:
-                eth_sortkey = rule.targetsort_awkfield - 1
-                decorated = [(line.split()[eth_sortkey], i, line) for i, line in enumerate(mklists_dict[target])]
+            # sort mkl_d[trg], dsu-style, if trgsort_awkf greater than zero (note: zero is false)
+            # in trgsort_awkf, zero means "no sort order specified"
+            if rule.trgsort_awkf:
+                eth_sortkey = rule.trgsort_awkf - 1
+                decorated = [(ln.split()[eth_sortkey], i, ln) for i, ln in enumerate(mkl_d[trg])]
                 decorated.sort()
-                mklists_dict[target] = [line for grade, i, line in decorated]
+                mkl_d[trg] = [ln for grade, i, ln in decorated]
         
-    return mklists_dict
+    return mkl_d
 
-if __name__ == "__main__":
-    from collections import namedtuple
-    Rule = namedtuple('Rule', 'sourcematch_awkfield source_matchregex source target targetsort_awkfield')
-    globlines_list1 = [ 'two ticks\n', 'an ant\n', 'the mite\n' ]
-    rules_list1 = [ Rule(2, 'i', 'a.txt', 'b.txt', 0) ]
-    x = shuffle(rules_list1, globlines_list1)
-    print(x)
-
-    """
-        if sort_order > 0:
-            mklists_dict[target] according to targetsort_awkfield
-    """
--- tests/test_new_shuffle.py
+++ tests/test_new_shuffle.py
@@ -2,16 +2,16 @@ import pytest
 from new_shuffle import shuffle
 from collections import namedtuple
 
-Rule = namedtuple('Rule', 'sourcematch_awkfield source_matchregex source target targetsort_awkfield')
+Rule = namedtuple('Rule', 'srcmatch_awkf srcmatch_rgx src trg trgsort_awkf')
 
 def test_shuffle():
-    rules_list1 = [ Rule(2, 'i', 'a.txt', 'b.txt', 0) ]
-    globlines_list1 = ['two ticks\n', 'an ant\n', 'the mite\n']
+    rules_l1 = [ Rule(2, 'i', 'a.txt', 'b.txt', 0) ]
+    globlines_l1 = ['two ticks\n', 'an ant\n', 'the mite\n']
     output = { 'a.txt': ['an ant\n'], 'b.txt': ['two ticks\n', 'the mite\n'] }
-    assert shuffle(rules_list1, globlines_list1) == output
+    assert shuffle(rules_l1, globlines_l1) == output
 
 def test_shuffle_sort():
-    rules_list1 = [ Rule(2, 'i', 'a.txt', 'b.txt', 1) ]
-    globlines_list1 = ['two ticks\n', 'an ant\n', 'the mite\n']
+    rules_l1 = [ Rule(2, 'i', 'a.txt', 'b.txt', 1) ]
+    globlines_l1 = ['two ticks\n', 'an ant\n', 'the mite\n']
     output = { 'a.txt': ['an ant\n'], 'b.txt': ['the mite\n', 'two ticks\n'] }
-    assert shuffle(rules_list1, globlines_list1) == output
+    assert shuffle(rules_l1, globlines_l1) == output
