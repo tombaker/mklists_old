d378dcd610b74b90745b9aa3902b29be6688d6e1
Tom Baker <tom@tombaker.org>
2017-09-09 09:22:23 +0200

    change directory structure

--- mklists/get_globlines.py
+++ mklists/get_globlines.py
@@ -1,9 +1,11 @@
-def get_globlines_list(cwd=os.getcwd()):
+def get_globlines(cwd=os.getcwd()):
     """Something like:
     globlines_list = []
     for file in glob.glob('*'):
         globlines_list.append(file.readlines())
     return globlines_list
     """
-    pass
+    return cwd
 
+if __name__ == "__main__":
+    get_globlines()
