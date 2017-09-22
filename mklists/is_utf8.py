ee725e796d74cf6bcbdb15f17e4808d0808d7959
Tom Baker <tom@tombaker.org>
2017-09-22 18:39:48 +0200

    moved test_mklists.py to tests/ directory - 3 tests still discovered and pass

--- mklists/is_utf8.py
+++
@@ -1,7 +0,0 @@
-def is_utf8(file):
-    class NotUTF8Error(SystemExit): pass
-    try:
-        open(file).read(512)
-    except UnicodeDecodeError as e:
-        raise NotUTF8Error(f'File {file} not UTF-8: convert or delete, then retry.') from e
-
