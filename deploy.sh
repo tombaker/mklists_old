ffef4cd21ac47df32a8800e48fd1986901a149ae
Tom Baker <tom@tombaker.org>
2017-12-20 16:31:18 +0100

    change file structure of project as per Okken book

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# This command creates (or refreshes) the website at https://tombaker.github.io/mklists/
+# The command must be run from the root directory of the https://github.com/tombaker/mklists/ repo
+# MkDocs must be installed - see http://www.mkdocs.org/#installation
+# Behind the scenes, MkDocs builds the docs and uses the ghp-import tool to commit them 
+# to the gh-pages branch and push the gh-pages branch to GitHub.
+
+mkdocs gh-deploy
