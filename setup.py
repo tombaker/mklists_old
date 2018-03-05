aff7296e8ba71206cbaea1f4afe55942498d946d
Tom Baker <tom@tombaker.org>
2018-03-05 09:27:07 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
-"""Setup file for mklists project."""
-
 from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f:.read()
 
 setup(
     name='mklists',
-    version='0.1.0',
-    license='MIT',
-    description='Rearrange plain text lists by tweaking rules',
+    version='0.1',
+    description='Rearrange plain-text lists by tweaking rules',
+    long_description=readme(),
     author='Tom Baker',
-    url=' https://github.com/tombaker/mklists ',
+    author_email='tom@tombaker.org',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing',
+    ],
+    license='MIT',
+    url='https://github.com/tombaker/mklists',
+    py_modules=['mklists'],
     packages=find_packages(where='src'),
+    include_package_data=True,
     package_dir={'': 'src'},
     install_requires=[
         'pyyaml', 
     ],
+    zip_safe=False,
 )
