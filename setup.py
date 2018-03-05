06000bdf083090027a0ac9be2ae1d6776958fbb0
Tom Baker <tom@tombaker.org>
2018-03-05 17:34:51 +0100

    move src/mklists to mklists, create mklists/cli.py, edit setup.py accordingly

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mklists',
-    version='0.1.0',
-    license='MIT',
-    description='Rearrange plain text lists by tweaking rules',
-    author='Tom Baker',
-    url=' https://github.com/tombaker/mklists ',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'pyyaml', 
+    name = 'mklists',
+    version = '0.1.0',
+    license = 'MIT',
+    description = 'Rearrange plain text lists by tweaking rules',
+    author = 'Tom Baker',
+    url = 'https://github.com/tombaker/mklists',
+    packages = find_packages(exclude=['tests']),
+    install_requires =[
+        'pyyaml', 'posixpath', 'click'
     ],
+    entry_points = """
+        [console_scripts]
+        mklists=mklists.cli:cli
+    """
 )
