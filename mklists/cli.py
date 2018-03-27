6559dc845f86cb3f93a4d09260fcbd4309fd09e7
Tom Baker <tom@tombaker.org>
2018-03-27 21:23:10 +0200

    mklists/cli.py is working

--- mklists/cli.py
+++ mklists/cli.py
@@ -1,31 +1,28 @@
 import os
+import re
 import sys
 
 import click
 from dataclasses import dataclass
+from posixpath import realpath
 
-print('Running mklists')
-print()
-print()
-
-
+@dataclass
 class ListFolder():
+    list_folder:       click.Path
+    backup_folder:     click.Path = '.mklists'
+    backup_depth:      int = 3
+    html_folder:       click.Path = '.html'
+    config:            click.File = '.mklistsrc'
+    rules:             click.File = '.rules'
+    with_verification: bool = True 
+    verbose:           bool = False
 
-    def __init__(self, cwd):
-        self.cwd = cwd
-        self.config = {}
-        self.verbose = False
-
-    def get_config(self, key, value):
-        self.config[key] = value
+    def get_config(self):
+        click.echo('@@@ get configuration')
         if self.verbose:
             click.echo('running verbose')
 
-    def __repr__(self):
-        return '<ListFolder %r>' % self.cwd
-
-
-pass_config = click.make_pass_decorator(ListFolder)
+pass_listfolder = click.make_pass_decorator(ListFolder)
 
 
 @click.group()
@@ -47,20 +44,33 @@ pass_config = click.make_pass_decorator(ListFolder)
               type=click.File('r'),
               help="Change rules [.rules]. Repeatable.")
 @click.option('--with-verification', is_flag=True,
-              help='Compares summed size before and after.')
+              help='Compare before size with after [True].')
 @click.option('--verbose', is_flag=True,
               help='Enable verbose mode.')
 @click.version_option('0.1')
 @click.pass_context
-def cli(ctx, list_folder, backup_folder, html_folder, config, rules, verbose):
+def cli(ctx, list_folder, backup_folder, backup_depth, html_folder, config, rules, with_verification, verbose):
     """Rearrange plain-text lists by tweaking rules
     """
     ctx.obj = ListFolder(os.path.abspath(list_folder))
     ctx.obj.verbose = verbose
 
+    click.echo(f'cli()                      {cli}')
+    click.echo(f'ctx.obj.__dir__()...       {[item for item in ctx.obj.__dir__() if not re.search("__", item)]}')
+    click.echo(f'ctx.obj.list_folder        {ctx.obj.list_folder}')
+    click.echo(f'ctx.obj.backup_folder      {ctx.obj.backup_folder}')
+    click.echo(f'ctx.obj.backup_depth       {ctx.obj.backup_depth}')
+    click.echo(f'ctx.obj.html_folder        {ctx.obj.html_folder}')
+    click.echo(f'ctx.obj.config             {ctx.obj.config}')
+    click.echo(f'ctx.obj.rules              {ctx.obj.rules}')
+    click.echo(f'ctx.obj.with_verification  {ctx.obj.with_verification}')
+    click.echo(f'ctx.obj.verbose            {ctx.obj.verbose}')
+    click.echo(f'ctx.obj.get_config         {ctx.obj.get_config}')
+    click.echo()
+
 
 @cli.command()
-@pass_config
+@pass_listfolder
 def init(list_folder):
     """Initialize list folder.
 
@@ -73,15 +83,15 @@ def init(list_folder):
 @cli.command()
 @click.option('--backup-depth', default=3, 
               help='Retains max number of backups [3].')
-@pass_config
+@pass_listfolder
 def run(repo, backup_depth):
     """Remake lists.
     """
 
 
 @cli.command()
-@pass_config
-def check(ctx, files, message):
+@pass_listfolder
+def check(ctx):
     """Dry-run with verbose sanity checks.
     """
 
---
+++ tester.sh
@@ -0,0 +1,14 @@
+# clear; echo Running: mklists
+# mklists
+# read
+
+clear; echo Running: mklists check
+mklists check
+read
+
+clear; echo Running: mklists init
+mklists init
+read
+
+clear; echo Running: mklists run
+mklists run
