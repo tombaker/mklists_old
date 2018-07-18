169caed3e3c8b9ca39e145e7b5abb83ec4701b88
Tom Baker <tom@tombaker.org>
2018-07-18 13:00:08 +0200

    rules.py - turn help text into constant

--- mklists/rules.py
+++ mklists/rules.py
@@ -3,29 +3,45 @@ import sys
 from textwrap import dedent
 from dataclasses import dataclass
 
-class RuleFile(object):
-    """ print(rulefilename)
-        NoRuleFileError('Rule file does not exist or is unusable.')
-    """
-
 class BadRuleString(SystemExit):
+    pass
 
-    def __str__(self):
-        return """\
-                Example: '3 /x/ a.txt b.txt 2'
+VALID_CHARS = '@:-_=.{}{}'.format(string.ascii_letters, string.digits)
 
-                for each line in source a.txt
-                    if regex /x/ matches third field (the "match field")
-                        move line from a.txt to b.txt
-                        sort b.txt on second field
+CORRECT_RULE_FORM = """\
+The rule above is incorrectly formed.
 
-                Note:
-                -- if match field is 0, entire line (with whitespace) matched
-                -- if match field is greater than line length, match is ignored
-                -- regex must escape forward slashes - eg "/\/n/"
-                -- regex may include spaces - eg "/^From /"
-                """
+Rule must have four or (optionally) five fields:
 
+    3 /x/ a.txt b.txt
+    3 /x/ a.txt b.txt 2   # with optional sort order
+
+Fields:
+
+    1. Field against which regex is matched (a digit).
+    2. Regex, delimited by slashes.
+    3. Source file, lines of which are tested for matches.
+    4. Target file, destination of matching lines.
+    5. Sort order of target file
+
+In pseudo-code:
+
+    for each line in source a.txt
+        if regex /x/ matches third field (the "match field")
+            move line from a.txt to b.txt
+            sort b.txt on second field (if optional sort order given)
+        if regex /x/ does not match third field
+            do nothing
+
+Note:
+-- If match field is 0, entire line (with whitespace) matched.
+-- If match field is greater than line length, match is ignored.
+-- Regex must escape forward slashes - eg "/\/n/".
+-- Regex may include spaces - eg "/^From /"
+-- Whitespace is ignored except within regex (between first and last slash).
+-- Comments (everything after a pound sign) are ignored.
+-- Fields 3 and 4 are (configurably) valid filenames not containing slashes.
+"""
 
 def get_stringrules(*rules_files):
     stringrules = []
@@ -65,23 +81,47 @@ def listrule_to_stringrule(listrule):
         return repr(listrule)
     return " ".join([item for item in listrule])
 
-def check_listrules(listrules):
-    """2018-07-17: does not 'see' the liststring
-    Could test for content of error message too:
-    https://stackoverflow.com/questions/30256332/verify-the-the-error-code-or-message-from-systemexit-in-pytest
-    """
-    for listrule in listrules:
-        if not listrule[0].isdigit():
-            stringrule = " ".join([listrule[0], "/", listrule[1], "/", listrule[2], listrule[3], listrule[4]])
-            print(repr(stringrule))
-            raise BadRuleString
-        listrule[0] = int(listrule[0])
-    return listrules
+def exit_for_bad_rule_form(listrule_tested, message):
+    if len(listrule_tested) > 1:
+        listrule_tested[1] = '/' + listrule_tested[1] + '/'
+    print(" ".join(listrule_tested))
+    sys.exit(message)
 
-# Entire line must be five fields long
-# Fields 1 and 5 must be digits
-#    if rule[0].isdigit
-#    if rule[4].isdigit
+def check_listrules(listrules):
+    listrules_checked = []
+    for listrule_to_check in listrules:
+
+        # listrule_to_check must have 4 or 5 fields
+        if 4 <= len(listrule_to_check) <= 5:
+            exit_for_bad_rule_form(listrule_to_check, CORRECT_RULE_FORM)
+
+        # field 1 is a digit
+        if not listrule_to_check[0].isdigit():
+            exit_for_bad_rule_form(listrule_to_check, CORRECT_RULE_FORM)
+
+        # field 2 is a valid regular expression
+        try:
+            re.compile(listrule_to_check[1])
+        except:
+            exit_for_bad_rule_form(listrule_to_check, CORRECT_RULE_FORM)
+
+        # field 3 is a valid filename (according to set of valid characters)
+        if not all(c in VALID_CHARS for c in listrule_to_check[2]):
+            exit_for_bad_rule_form(listrule_to_check, CORRECT_RULE_FORM)
+
+        # field 4 is a valid filename (according to set of valid characters)
+        if not all(c in VALID_CHARS for c in listrule_to_check[3]):
+            exit_for_bad_rule_form(listrule_to_check, CORRECT_RULE_FORM)
+
+        # field 5 is a digit
+        if len(listrule_to_check) == 5:
+            if not listrule_to_check[4].isdigit():
+                exit_for_bad_rule_form(listrule_to_check, CORRECT_RULE_FORM)
+
+        listrules_checked.append(listrule)
+
+    return listrules_checked
+
 # Field 2, a regex
 #    def exit_rule_regex_must_be_escaped(line):
 #        print("In rule: %r" % line)
@@ -89,18 +129,10 @@ def check_listrules(listrules):
 #        print("...the rule component must be escaped as follows: %r" % re.escape(linesplitonorbar[1])
 # No source 
 #    probably because not yet created by target
-# $1.isdigit()  - which includes zero
-# $5.isdigit()  - which includes zero
-# $1 = int($1)
-# $5 = int($1)
-# except SourceMatchAndTargetSortOrderDigits:
 # $3 = re.compile($3)
-# $4 and $5:
-# must have only permitted characters
-# valid_chars = '@:-_=.{}{}'.format(string.ascii_letters, string.digits)
-# if it gets this far:
-# append line to rules_l
-
+# filenames must have only permitted characters
+# try:
+#     re.compile('he(lo')
 
 @dataclass
 class Rule:
@@ -112,3 +144,13 @@ class Rule:
     trgsort_awkf
     """
 
+# test for content of error message too:
+# https://stackoverflow.com/questions/30256332/verify-the-the-error-code-or-message-from-systemexit-in-pytest
+
+# In [6]: try:
+# ...:     re.compile(pattern)
+# ...: except:
+# ...:     print('oops')
+
+        # listrule_checked[0] = int(listrule_checked[0])
+        # listrule_checked[4] = int(listrule_checked[4])
