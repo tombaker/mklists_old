c1ad04c46a633241797d4bdf106b0676a0b25a85
Tom Baker <tom@tombaker.org>
2018-08-06 16:59:37 +0200

    rule.py - create register_target, but no tests yet - 3 tests still pass

--- mklists/rule.py
+++ mklists/rule.py
@@ -15,9 +15,35 @@ class Rule:
     target: str = None
     target_sortorder: int = 0
 
+    initialized = False
+    targets = []
 
     def has_five_fields(self):
         if len([v for v in self.__dict__.values() if v is not None]) != 5:
             raise NotFiveFieldsError
-            #sys.exit()
 
+    def register_source(self):
+        """\
+        When class is first created, class variables 'initialized' 
+        (a flag) and 'targets' (a list) are initialized.
+
+        When Rule instantiated for first time: 
+        * value of self.target added to 'targets' list
+        * value of self.source _also_ added to 'targets' list 
+        * 'initialized' flag is set to True
+
+        When Rule is instantiated each subsequent time
+        * iff value of self.source is in 'targets' list
+          * value of self.target added to 'targets' list
+        * iff value of self.source is _not_ in 'targets' list
+          * exit with error message
+
+        @@@Todo: create 
+        """
+        if not Rule.initialized:
+            Rule.targets.append(self.source)
+            Rule.initialized = True
+        if self.source not in Rule.targets:
+            print(f"oh no! {self.source} is not in Rule.targets!")
+        Rule.targets.append(self.target)
+        print(Rule.targets)
