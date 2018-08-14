f2c95477f48b00c9d3c05452cecc91bbac0ced47
Tom Baker <tom@tombaker.org>
2018-08-14 21:21:11 +0200

    cli.py - some progress with pylint

--- mklists/cli.py
+++ mklists/cli.py
@@ -1,56 +1,57 @@
-import os
-import re
-import glob
+"""docstring"""
 
-import click
 from dataclasses import dataclass
 from configparser import ConfigParser
+import click
 
 
 @click.group()
 @click.option(
-    '--config', 
-    default='.mklistsrc', 
-    metavar='FILENAME', 
+    '--config',
+    default='.mklistsrc',
+    metavar='FILENAME',
     help="Configuration file.")
 @click.option(
-    '--rules', 
-    default='.rules', 
-    metavar='FILENAME', 
+    '--rules',
+    default='.rules',
+    metavar='FILENAME',
+    multiple=True,
     help="Rule file, repeatable.")
 @click.option(
-    '--data-folder', 
-    default='.', 
-    metavar='DIRNAME', 
+    '--data-folder',
+    default='.',
+    metavar='DIRNAME',
     help="Data folder.")
 @click.option(
-    '--html-folder', 
-    default='.html', 
-    metavar='DIRNAME', 
+    '--html-folder',
+    default='.html',
+    metavar='DIRNAME',
     help="Data folder, urlified.")
 @click.option(
-    '--backup-folder', 
-    default='.backups', 
-    metavar='DIRNAME', 
+    '--backup-folder',
+    default='.backups',
+    metavar='DIRNAME',
     help="Backup folder.")
 @click.option(
-    '--backup-depth', 
-    default=3, 
+    '--backup-depth',
+    default=3,
     help="Backup depth.")
 @click.option(
-    '--verbose', 
-    default=False, 
+    '--verbose',
+    default=False,
     is_flag=True,
     help="Run in verbose mode.")
 @click.version_option('0.2', help="Show version and exit.")
 @click.pass_context
-def cli(ctx, config, rules, data_folder, html_folder, backup_folder, 
+def cli(ctx, config, rules, data_folder, html_folder, backup_folder,
         backup_depth, verbose):
     """Manage plain text lists by tweaking rules"""
+
     print("Before reading config file: backup_depth =", backup_depth)
     mklrc = ConfigParser()
     mklrc.read('.mklistsrc')
-    mklistsrc = dict([[key, mklrc['DEFAULTS'][key]] for key in mklrc['DEFAULTS']])
+    # mklistsrc = dict([[key, mklrc['DEFAULTS'][key]] for key in mklrc['DEFAULTS']])
+    mklistsrc = {[key, mklrc['DEFAULTS'][key]] for key in mklrc['DEFAULTS']}
     print("Values as read from config file: mklistsrc = ", mklistsrc)  ################
     ctx.obj = mklistsrc
     print("After reading values from config file: ctx.obj = ", ctx.obj)  ################
@@ -64,7 +65,7 @@ def cli(ctx, config, rules, data_folder, html_folder, backup_folder,
     if data_folder:
         ctx.obj['data_folder'] = data_folder
 
-    if html_folder:   
+    if html_folder:
         ctx.obj['html_folder'] = html_folder
 
     if backup_folder:
@@ -77,7 +78,9 @@ def cli(ctx, config, rules, data_folder, html_folder, backup_folder,
         ctx.obj['verbose'] = verbose
 
     print("ctx.obj = ", ctx.obj)  ################ 12345
-    print("After overriding with values set on command line: ctx.obj['data_folder'] =", repr(ctx.obj['data_folder']))  #######
+    print("After overriding with values set on command line:"
+          "ctx.obj['data_folder'] =", repr(ctx.obj['data_folder']))  #######
+
     #click.echo(f'>>> ctx.obj.backup_depth      =>  {ctx.obj.backup_depth}') #####
 
 
@@ -94,7 +97,6 @@ def make(ctx):
     click.echo(f'>>> ctx                             =>  {ctx}')
     click.echo(f'>>> ctx.obj                        =>  {ctx.obj}')
     click.echo(f'>>> ctx.obj["rules"]               =>  {ctx.obj["rules"]}')
-    #click.echo(f'>>> ctx.__dir__()...              =>  {[item for item in ctx.__dir__() if not re.search("__", item)]}')
     #click.echo(f'>>> ctx.list_folder               =>  {ctx.list_folder}')
     #click.echo(f'>>> ctx.backup_folder             =>  {ctx.backup_folder}')
     #click.echo(f'>>> ctx.backup_depth              =>  {ctx.backup_depth}')
@@ -107,11 +109,10 @@ def make(ctx):
 @click.pass_context
 def check(ctx):
     """Dry-run with verbose sanity checks."""
-    # Print to screen all (non-dunder) config attributes/values
+    print(f"mklists check: ctx.obj is {repr(ctx.obj)}")
 
 @cli.command()
 @click.pass_context
 def rules(ctx):
     """Inspect rules."""
     # Print to screen all (non-dunder) config attributes/values
-
