aee0869642faf4ffc38b15499cddc675d6c5c425
Tom Baker <tom@tombaker.org>
2018-08-16 09:30:52 +0200

    improve command-line display

--- mklists/cli.py
+++ mklists/cli.py
@@ -18,109 +18,131 @@ import yaml
 
 
 @click.group()
-@click.option('--config', type=str, metavar='FILENAME', help="Configuration file.")
-@click.option('--rules', type=str, metavar='FILENAME', multiple=True, help="Rule file, repeatable.")
-@click.option('--data-folder', type=str, metavar='DIRNAME', help="Data folder.")
-@click.option('--html-folder', type=str, metavar='DIRNAME', help="Data folder, urlified.")
-@click.option('--backup-folder', type=str, metavar='DIRNAME', help="Backup folder.")
-@click.option('--backup-depth', type=int, help="Backup depth.")
-@click.option('--verbose', type=bool, is_flag=True, help="Run in verbose mode.")
-@click.version_option('0.2', help="Show version and exit.")
+@click.option('--config', type=str, metavar='FILENAME', 
+              help="Config file [.mklistsrc]")
+@click.option('--rules', type=str, metavar='FILENAME', multiple=True, 
+              help="Rule file - repeatable [.rules]")
+@click.option('--data', type=str, metavar='DIRNAME', 
+              help="Data folder [.]")
+@click.option('--html', type=str, metavar='DIRNAME', 
+              help="Data folder, urlified [.html]")
+@click.option('--backups', type=str, metavar='DIRNAME', 
+              help="Backup folder [.backups]")
+@click.option('--backup-depth', type=int, metavar='INT',
+              help="Backup depth [3]")
+@click.option('--verbose', type=bool, is_flag=True, 
+              help="Run verbosely")
+@click.version_option('0.2', help="Show version and exit")
+@click.help_option(help="Show help and exit")
 @click.pass_context
-def cli(ctx, config, rules, data_folder, html_folder, backup_folder, backup_depth, verbose):
+def cli(ctx, config, rules, data, html, backups, 
+        backup_depth, verbose):
     """Manage plain text lists by tweaking rules"""
 
+    if verbose:
+        print('Printing diagnostic information.')
 
-    # Sensible default configuration values (eg, in absence of config file)
+    # Hardwired config values (before reading optional config file)
     ctx.obj = {
         'config': '.mklistsrc',
-        'rules': ('.rules',),
-        'data_folder': '.',
-        'html_folder': '.html',
-        'backup_folder': '.backups',
+        'rules': ['.rules',],
+        'data': '.',
+        'html': '.html',
+        'backups': '.backups',
         'backup_depth': 3,
         'verbose': False}
 
-    # if command line option points to different config file, use that one
-    default_configfile_specified = True
-    if config:
-        ctx.obj['config'] = config
-        default_configfile_specified = False
+    if verbose:
+        click.echo('Hardwired config defaults:')
+        for key, value in ctx.obj.items():
+            print("    ", key, "=", value)
 
-    # Confirm default config values
-    print("Default config dict saved on ctx.obj = ", ctx.obj)  ################
+    # If command line specifies non-default config file, use it
+    if config:
+        using_nondefault_configfile = True
+        ctx.obj['config'] = config
+        if verbose:
+            print(f"Will use config file, {repr(ctx.obj['config'])}.")
+    else:
+        using_nondefault_configfile = False
 
     # Try to read configfile and use it to update ctx.obj
     # If configfile does not exist, write ctx.obj to a YAML file
     try:
-        with open(config) as configfile:
+        with open(ctx.obj['config']) as configfile:
             ctx.obj.update(yaml.load(configfile))
+        if verbose:
+            print('Config settings after reading config file:')
+            for key, value in ctx.obj.items():
+                print("    ", key, "=", value)
     except FileNotFoundError:
-        if default_configfile_specified:
-            print("Creating config file '.mklistsrc'. Can be edited.")
-            yaml.safe_dump(ctx.obj, sys.stdout, default_flow_style=False) # pretty-prints
+        if using_nondefault_configfile:
+            raise ConfigFileNotFoundError(f"File {repr(config)} not found.")
         else:
-            raise ConfigFileNotFoundError(f"{repr(config)} not found.")
+            print("Warning: No config file found; using hardwired defaults.")
 
-    # if rules:
-    #     ctx.obj['rules'] = rules
+    if rules:
+        ctx.obj['rules'] = list(rules)
+        if verbose:
+            print(f"Will use rule file(s) {repr(ctx.obj['rules'])}.")
 
-    # if data_folder:
-    #     ctx.obj['data_folder'] = data_folder
+    if data:
+        ctx.obj['data'] = data
+        if verbose:
+            print(f"Will use data folder {repr(ctx.obj['data'])}.")
 
-    # if html_folder:
-    #     ctx.obj['html_folder'] = html_folder
+    if html:
+        ctx.obj['html'] = html
+        if verbose:
+            print(f"Will use urlified data folder {repr(ctx.obj['html'])}.")
 
-    # if backup_folder:
-    #     ctx.obj['backup_folder'] = backup_folder
+    if backups:
+        ctx.obj['backups'] = backups
+        if verbose:
+            print(f"Will use backups folder {repr(ctx.obj['html'])}.")
 
     if backup_depth:
         ctx.obj['backup_depth'] = backup_depth
+        if verbose:
+            print(f"Will keep last {repr(ctx.obj['backup_depth'])} backups.")
 
-    print("ctx.obj with backup_depth overriden by click option = ", ctx.obj)  ################
-
-    # if verbose:
-    #     ctx.obj['verbose'] = verbose
-
-    # print("ctx.obj = ", ctx.obj)  ################ 12345
-    # print("After overriding with values set on command line:"
-    #       "ctx.obj['backup_depth'] =", repr(ctx.obj['backup_depth']))  #######
-
-    # #click.echo(f'>>> ctx.obj.backup_depth      =>  {ctx.obj.backup_depth}') #####
-
+    if verbose:
+        ctx.obj['verbose'] = verbose
+        print('Final config settings:')
+        for key, value in ctx.obj.items():
+            print("    ", key, "=", value)
 
 @cli.command()
 @click.pass_context
 def init(ctx):
-    """Initialize folder for Mklists data."""
-    print('@@@TODO')
+    """Initialize data folder"""
+
+    print(f"Creating mandatory default rule file: '.rules'.")
+    print(f"Creating optional config file {repr(ctx.obj['config'])}.")
+    # yaml.safe_dump(ctx.obj, sys.stdout, default_flow_style=False)
 
 @cli.command()
 @click.pass_context
 def make(ctx):
-    """Regenerate lists according to rules."""
-    click.echo(f'>>> ctx                             =>  {ctx}')
-    click.echo(f'>>> ctx.obj                        =>  {ctx.obj}')
-    click.echo(f'>>> ctx.obj["rules"]               =>  {ctx.obj["rules"]}')
-    #click.echo(f'>>> ctx.list_folder               =>  {ctx.list_folder}')
-    #click.echo(f'>>> ctx.backup_folder             =>  {ctx.backup_folder}')
-    #click.echo(f'>>> ctx.backup_depth              =>  {ctx.backup_depth}')
-    #click.echo(f'>>> ctx.html_folder               =>  {ctx.html_folder}')
-    #click.echo(f'>>> ctx.verbose                   =>  {ctx.verbose}')
-    #click.echo(f'>>> ctx.get_config                =>  {ctx.get_config}')
-    #click.echo()
+    """Remake lists as per rules"""
+
+    print(f"* Get rules: {repr(ctx.obj['rules'])}.")
+    print(f"* Check data folder: {repr(ctx.obj['data'])}.")
+    print(f"* Get data: {repr(ctx.obj['data'])}.")
+    print(f"* Apply rules to data, modifying data dictionary.")
+    print(f"* Double-check for data loss??")
+    print(f"* Create (or use) time-stamped folder within backup folder.")
+    print(f"* Move files to time-stamped backup folder.")
+    print(f"* Write out data dictionary as files in data folder.")
 
 @cli.command()
 @click.pass_context
-def check(ctx):
-    """Dry-run with verbose sanity checks."""
-    print(f"mklists check: ctx.obj is {repr(ctx.obj)}")
+def verify(ctx):
+    """Check rules and data folder, verbosely"""
 
-@cli.command()
-@click.pass_context
-def rules(ctx):
-    """Inspect rules."""
-    # Print to screen all (non-dunder) config attributes/values
+    print(f"* Get rules: {repr(ctx.obj['rules'])}, verbosely.")
+    print(f"* Check data folder {repr(ctx.obj['data'])}, verbosely.")
+
 
 class ConfigFileNotFoundError(SystemExit):
     """Specified (non-default) configuration file was not found"""
