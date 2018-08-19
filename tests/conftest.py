e2b7607ecad487bb7cbc1e6efd12412f9cb32764
Tom Baker <tom@tombaker.org>
2018-08-19 10:04:13 +0200

    delete non-YAML-style rule files, first two passing tests using rulefile fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,35 @@
-rules = [['0', 'i', 'a.txt', 'b.txt', '0']]
-lines = ['two ticks\n', 'an ant\n', 'the mite\n']
-output = {'a.txt': ['an ant\n'], 'b.txt': ['two ticks\n', 'the mite\n']}
+import pytest
+import yaml
+from textwrap import dedent
 
-rules = [['2', 'i', 'a.txt', 'b.txt', '1']]
-lines = ['two ticks\n', 'an ant\n', 'the mite\n']
-output = {'a.txt': ['an ant\n'], 'b.txt': ['the mite\n', 'two ticks\n']}
+@pytest.fixture(scope='module')
+def rule_yaml(tmpdir_factory):
+    """Write some YAML-formatted rules to YAML rule files."""
+
+    yaml_rule_data = """\
+    - [0,  '.'        , lines        , __RENAME__   , 0]
+    - [0,  '^= 20'    , __RENAME__   , calendar     , 1]
+    - [0   , 'NOW'    , lines        , __RENAME__   , 0]
+    - [0   , 'LATER'  , __RENAME__   , calendar     , 1]
+    """
+
+    rules = tmpdir_factory.mktemp('datadir').join('rules')
+    print(f"Created 'rules': {repr(rules)}")
+    rules.write(dedent(yaml_rule_data))
+
+    return rules
+
+
+
+
+
+
+
+
+#rules = [['0', 'i', 'a.txt', 'b.txt', '0']]
+#lines = ['two ticks\n', 'an ant\n', 'the mite\n']
+#output = {'a.txt': ['an ant\n'], 'b.txt': ['two ticks\n', 'the mite\n']}
+
+#rules = [['2', 'i', 'a.txt', 'b.txt', '1']]
+#lines = ['two ticks\n', 'an ant\n', 'the mite\n']
+#output = {'a.txt': ['an ant\n'], 'b.txt': ['the mite\n', 'two ticks\n']}
---
+++ tests/test_rule2.py
@@ -0,0 +1,87 @@
+import pytest
+import os
+from mklists.rule import Rule
+
+def test_rulesy(tmpdir, use_rules):
+    os.chdir(tmpdir)
+    # assert os.path.isfile('.rulesy')
+    assert os.path.cwd() == tmpdir
+
+@pytest.mark.skip
+def test_source_is_registered():
+    x = Rule('1', 'NOW', 'a.txt', 'b.txt', '0')
+    Rule.register(x)
+    y = Rule('1', 'LATER', 'b.txt', 'c.txt', '0')
+    assert Rule.register(y)
+
+@pytest.mark.skip
+def test_source_is_registered_not():
+    x = Rule('1', 'NOW', 'a.txt', 'b.txt', '0')
+    Rule.register(x)
+    y = Rule('1', 'LATER', 'c.txt', 'd.txt', '0')
+    with pytest.raises(SystemExit):
+        Rule.register(y)
+
+@pytest.mark.rule
+def test_is_valid_not():
+    x = Rule(1, 'N(OW', 'a', 'b', 2)
+    with pytest.raises(SystemExit):
+        x.is_valid()
+
+@pytest.mark.rule
+def test_rule():
+    x = Rule(1, '.', 'a', 'b', 2)
+    assert x.source == 'a'
+
+@pytest.mark.rule
+def test_rulestring_regex_has_space():
+    x = Rule(1, '^X 19', 'a', 'b', 2)
+    assert x.source_matchpattern == '^X 19'
+
+@pytest.mark.rule
+def test_source_filename_valid():
+    x = Rule('1', '^X 19', 'a.txt', 'b.txt', '2')
+    assert x._source_and_target_filenames_are_valid()
+
+@pytest.mark.rule
+def test_target_filename_valid():
+    x = Rule('1', '^X 19', 'a.txt', 'b.txt', '2')
+    assert x._source_and_target_filenames_are_valid()
+
+@pytest.mark.rule
+def test_target_filename_valid_not():
+    x = Rule('1', '^X 19', 'a.txt', 'b^.txt', '2')
+    with pytest.raises(SystemExit):
+        x._source_and_target_filenames_are_valid()
+
+@pytest.mark.rule
+def test_source_matchfield_and_target_sortorder_are_valid():
+    x = Rule('1', 'NOW', 'a.txt', 'b.txt', '0')
+    assert x._source_matchfield_and_target_sortorder_are_integers
+
+@pytest.mark.rule
+def test_source_matchfield_and_target_sortorder_are_integers():
+    x = Rule('1', 'NOW', 'a.txt', 'b.txt', '0')
+    assert x._source_matchfield_and_target_sortorder_are_integers
+
+@pytest.mark.rule
+def test_source_ne_target():
+    x = Rule('1', 'NOW', 'a.txt', 'b.txt', '0')
+    assert x._source_is_not_equal_target
+
+@pytest.mark.rule
+def test_source_ne_target_not():
+    x = Rule('1', 'NOW', 'a.txt', 'a.txt', '0')
+    with pytest.raises(SystemExit):
+        x._source_is_not_equal_target()
+
+@pytest.mark.rule
+def test_source_matchpattern_is_valid():
+    x = Rule('1', 'NOW', 'a.txt', 'a.txt', '0')
+    assert x._source_matchpattern_is_valid
+
+@pytest.mark.rule
+def test_source_matchpattern_is_valid_not():
+    x = Rule('1', 'N(OW', 'a.txt', 'a.txt', '0')
+    with pytest.raises(SystemExit):
+        x._source_matchpattern_is_valid()
