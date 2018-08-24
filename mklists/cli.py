f1c68641534e8050fc1b8c851c2005022881ae90
Tom Baker <tom@tombaker.org>
2018-08-24 18:34:35 +0200

    click interface - turn backup and urlify into flags

--- mklists/cli.py
+++ mklists/cli.py
@@ -11,34 +11,33 @@ import click
 import os
 from mklists import VALID_FILENAME_CHARS, MKLISTSRC
 
-
 @click.group()
 @click.option('--datadir', type=str, metavar='DIRPATH',
-              help="Set DIRPATH as data directory [./]")
+              help="Set DIRPATH as current data directory")
 @click.option('--rules', type=str, metavar='FILENAME', multiple=True,
-              help="Use non-default rule file - repeatable [.rules]")
-@click.option('--htmldir', type=str, metavar='DIRPATH',  # change to flag?
-              help="Copy data files, urlified, to DIRPATH")
-@click.option('--backupdir', type=str, metavar='DIRPATH',
-              help="Back up data files to DIRPATH")
+              help="Use non-default rule file - repeatable")
+@click.option('--backup', type=bool, is_flag=True,
+              help="Back up input data to .backups/YYYYMMDD_HHMMSS/")
+@click.option('--urlify', type=bool, is_flag=True,
+              help="Copy output data, urlified, to .urlified/")
 @click.option('--verbose', type=bool, is_flag=True, help="Run verbosely")
-@click.option('--vverbose', type=bool, is_flag=True, help="Run very verbosely")
 @click.version_option('0.1.3', help="Show version and exit")
 @click.help_option(help="Show help and exit")
 @click.pass_context
-def cli(ctx, datadir, rules, htmldir, backupdir, verbose, vverbose):
-    """Tweak plain-text todo lists by rules"""
+def cli(ctx, datadir, rules, backup, urlify, verbose):
+    """Evolve plain-text todo lists by tweaking rules"""
 
-    if vverbose:
-        print('Printing diagnostic information.')
-        print(VALID_FILENAME_CHARS)
+    if verbose:
+        print('Running in verbose mode.')
 
     # Hardwired default values (before reading '.mklistsrc')
     ctx.obj = {
         'rules': ['.rules', ],
         'datadir': '.',
-        'htmldir': '.html',
-        'backupdir': '.backups',
+        'urlify': False,
+        'urlify_dir': '.urlified',
+        'backup': False,
+        'backup_dir': '.backups',
         'backup_depth': 3,
         'verbose': False,
         'verbose': False,
@@ -49,15 +48,13 @@ def cli(ctx, datadir, rules, htmldir, backupdir, verbose, vverbose):
     if datadir:
         session_datadir = datadir
         try:
-            print(f"Changing to working directory {repr(session_datadir)}.")
             os.chdir(session_datadir)
+            if verbose:
+                print(f"Changing to working directory {repr(session_datadir)}.")
         except FileNotFoundError:
             raise DatadirNotAccessibleError(f"{datadir} not accessible.")
 
-    ### CHANGE HERE TO WORKING DIRECTORY - test:
-    print(os.getcwd())
-
-    if vverbose:
+    if verbose:
         click.echo('Hardwired configuration defaults:')
         for key, value in ctx.obj.items():
             print("    ", key, "=", value)
@@ -69,7 +66,7 @@ def cli(ctx, datadir, rules, htmldir, backupdir, verbose, vverbose):
     except FileNotFoundError:
         raise ConfigFileNotFoundError(f"{repr(MKLISTSRC)} not found.")
 
-    if vverbose:
+    if verbose:
         print('Config settings after reading config file:')
         for key, value in ctx.obj.items():
             print("    ", key, "=", value)
@@ -81,15 +78,17 @@ def cli(ctx, datadir, rules, htmldir, backupdir, verbose, vverbose):
             for rulefile in rules:
                 print(f"Will use rule file {repr(rulefile)}.")
 
-    if htmldir:
-        ctx.obj['htmldir'] = htmldir
+    if urlify:
+        ctx.obj['urlify'] = urlify
+        urlify_dir = ctx.obj['urlify_dir']
         if verbose:
-            print(f"Will copy new data files, urlified, to {repr(htmldir)}.")
+            print(f"Will mirror output data, urlified, to {repr(urlify_dir)}.")
 
-    if backupdir:
-        ctx.obj['backupdir'] = backupdir
+    if backup:
+        ctx.obj['backup'] = backup
+        backup_dir = ctx.obj['backup_dir']
         if verbose:
-            print(f"Will back up existing data files to {repr(backupdir)}.")
+            print(f"Will back up input data to {repr(backup_dir)}.")
 
     if verbose:
         ctx.obj['verbose'] = verbose
@@ -101,7 +100,7 @@ def cli(ctx, datadir, rules, htmldir, backupdir, verbose, vverbose):
 @cli.command()
 @click.pass_context
 def init(ctx):
-    """Initialize data folder"""
+    """Initialize data folder (or reset to defaults)"""
 
     print(f"Create basic '.rules' or, if already exists, ask to replace.")
     print(f"Create '.mklistsrc' or, if already exists, ask to replace.")
@@ -112,19 +111,28 @@ def init(ctx):
 
 
 @cli.command()
+@click.option('--urlify_dir', type=str, metavar='DIRPATH',
+              help="Copy data files, urlified, to DIRPATH")
+@click.option('--backup-dir', type=str, metavar='DIRPATH',
+              help="Back up data files to DIRPATH")
 @click.pass_context
-def make(ctx):
-    """Remake lists as per rules"""
+def make(ctx, urlify_dir, backup_dir):
+    """Process lists as per rules"""
+
+    session_datadir = ctx.obj['datadir']
+    urlify_dir = ctx.obj['urlify_dir']
+    backup_dir = ctx.obj['backup_dir']
 
     print(f"* Already got configuration in main mklists command.")
-    print(f"* Already set datadir to current working directory.")
-    print(f"* Initialize datadict from with datalines from files in datadir.")
-    print(f"* Get rules from rulefiles (in principle anywhere).")
+    print(f"* Already set datadir to {os.getcwd()}.")
+    for rulefile in ctx.obj['rules']:
+        print(f"* Get rules from file {repr(rulefile)}.")
+    print(f"* Get datalines from files in {repr(session_datadir)}.")
     print(f"* Apply rules to datalines, modifying in-memory datadict.")
-    print(f"* Backup option: Create time-stamped backupdir.")
-    print(f"* Backup option: Move existing files to backupdir.")
+    print(f"* Backup option: Create time-stamped backup_dir.")
+    print(f"* Backup option: Move existing files to backup_dir.")
     print(f"* Write out datadict values as files in datadir.")
-    print(f"* HTML option: Write out datadict values as files in htmldir.")
+    print(f"* HTML option: Write out datadict values as files in urlify_dir.")
     print(f"* Move files outside datadir as per ['files2dirs'].")
 
 
@@ -133,13 +141,9 @@ def make(ctx):
 def verify(ctx):
     """Check rules and data folder, verbosely"""
 
-    rules = ctx.obj['rules']
-
-    for rulefile in rules:
+    for rulefile in ctx.obj['rules']:
         print(f"* Get rules from file {repr(rulefile)}, verbosely.")
     print(f"* Check data folder {repr(ctx.obj['datadir'])}, verbosely.")
-
-    print(f"We are still in {os.getcwd()}.")
 
 
 class ConfigError(SystemExit):
