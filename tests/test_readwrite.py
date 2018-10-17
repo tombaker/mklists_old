52254353179dbe2b52f95d71add934c957136cc7
Tom Baker <tom@tombaker.org>
2018-10-17 17:12:02 +0200

    tests/test_readwrite.py - test of write_initial_globalrules passes!!

--- tests/test_readwrite.py
+++ tests/test_readwrite.py
@@ -2,6 +2,22 @@ import pytest
 import os
 from mklists import (MKLISTSRC, STARTER_GLOBALRULES, 
     STARTER_GLOBALRULES, STARTER_LOCALRULES)
+from mklists.readwrite import write_initial_rulefiles
+
+
+@pytest.mark.tmpdir
+def test_change_directory(tmpdir):
+    os.chdir(tmpdir)
+    assert str(tmpdir) == os.getcwd()
+
+@pytest.mark.tmpdir
+def test_write_initial_rules(tmpdir):
+    os.chdir(tmpdir)
+    write_initial_rulefiles(global_rules_filename='.globalrules')
+    globalrules_name = tmpdir.join('.globalrules')
+    globalrules_content = open(globalrules_name).read()
+    globalrules_hash = hash(globalrules_content)
+    assert hash(STARTER_GLOBALRULES) == globalrules_hash
 
 @pytest.mark.init_config
 def test_init_configfile():
