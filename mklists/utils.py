9cc2e6837a2ca2fb6dc827a4411f89edf0df513b
Tom Baker <tom@tombaker.org>
2018-11-01 10:14:09 +0100

    utils.py - black-formatted

--- mklists/utils.py
+++ mklists/utils.py
@@ -3,14 +3,20 @@
 import os
 import re
 import glob
-from mklists import (URL_PATTERN, INVALID_FILENAME_PATS, 
-    VALID_FILENAME_CHARS, DatadirNotAccessibleError, 
-    DatadirHasNonFilesError, BadFileFormatError, 
-    BadFilenameError)
+from mklists import (
+    URL_PATTERN,
+    INVALID_FILENAME_PATS,
+    VALID_FILENAME_CHARS,
+    DatadirNotAccessibleError,
+    DatadirHasNonFilesError,
+    BadFileFormatError,
+    BadFilenameError,
+)
 
 
 def change_working_directory(dirname, verb=False):
     """Set current working directory for mklists (data)."""
+
     if dirname is not None:
         try:
             os.chdir(dirname)
@@ -19,19 +25,24 @@ def change_working_directory(dirname, verb=False):
         except FileNotFoundError:
             raise DatadirNotAccessibleError(f"{dirname} is not accessible.")
 
+
 def is_file(object_path):
     """Returns True if object is a file.
 
     Raises:
         DatadirHasNonFilesError: if object is not a file.
     """
+
     if not os.path.isfile(object_path):
         return False
     return True
 
-def has_valid_name(filename, 
-                   bad_patterns=INVALID_FILENAME_PATS,
-                   valid_chars=VALID_FILENAME_CHARS):
+
+def has_valid_name(
+    filename,
+    bad_patterns=INVALID_FILENAME_PATS,
+    valid_chars=VALID_FILENAME_CHARS,
+):
     """Return True if filename has no invalid characters or patterns.
 
     Used to block execution of mklists if the data
@@ -43,14 +54,17 @@ def has_valid_name(filename,
     """
     for bad_pattern in bad_patterns:
         if re.search(bad_pattern, filename):
-            print(f"Bad pattern {repr(bad_pattern)} "
-                  f"in filename {repr(filename)}.")
+            print(
+                f"Bad pattern {repr(bad_pattern)} "
+                f"in filename {repr(filename)}."
+            )
             return False
     for char in filename:
         if char not in valid_chars:
             print(f"{repr(char)} is not a valid filename character.")
             return False
     return True
+
 
 def has_valid_contents(filename):
     """Returns True if file is UTF8-encoded and has no blank lines.
@@ -71,6 +85,7 @@ def has_valid_contents(filename):
         raise BadFileFormatError(f"{repr(filename)} is not in UTF-8 format.")
     return True
 
+
 def _is_utf8_encoded(file):
     """Returns True if a data files is UTF8-encoded.
 
@@ -83,6 +98,7 @@ def _is_utf8_encoded(file):
         raise BadFileFormatError(f"{repr(filename)} is not in UTF-8 format.")
     return True
 
+
 def _has_no_blank_lines(text_file):
     """Returns True if file has no blank lines.
     
@@ -93,13 +109,15 @@ def _has_no_blank_lines(text_file):
                 return False
     return True
 
+
 def linkify(string_raw):
     """docstring"""
-    if '<a href=' in string_raw:
+    if "<a href=" in string_raw:
         return string_raw
     return re.compile(URL_PATTERN).sub(r'<a href="\1">\1</a>', string_raw)
+
 
 def ls_visible(cwd=os.getcwd()):
     """Do I need to break this out into separate function?"""
     os.chdir(cwd)
-    return [name for name in glob.glob('*')]
+    return [name for name in glob.glob("*")]
