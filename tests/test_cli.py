29bad2c138e74548dca034a4d8ee6a24e43a7dec
Tom Baker <tom@tombaker.org>
2018-11-02 08:13:13 +0100

    remove references to backup_dir and backup from tests/

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -37,14 +37,12 @@ def test_apply_overrides_from_file(cwd_configured):
 def test_apply_overrides():
     initial_context = {
         "ctx": "something",
-        "backup_dir": ".backups",
         "backup_depth": 1,
     }
     overrides_from_file = {"backup_depth": 500}
     updated_context = apply_overrides(initial_context, overrides_from_file)
     expected_context = {
         "ctx": "something",
-        "backup_dir": ".backups",
         "backup_depth": 500,
     }
     assert updated_context == expected_context
@@ -54,17 +52,14 @@ def test_apply_overrides():
 def test_apply_overrides2():
     updated_context = {
         "ctx": "something",
-        "backup_dir": ".backups",
         "backup_depth": 500,
     }
     overrides_from_cli = {
-        "backup_dir": None,
         "backup_depth": 1000,
     }
     updated_context2 = apply_overrides(updated_context, overrides_from_cli)
     expected_context = {
         "ctx": "something",
-        "backup_dir": ".backups",
         "backup_depth": 1000,
     }
     assert updated_context2 == expected_context
@@ -73,7 +68,7 @@ def test_apply_overrides2():
 @pytest.mark.cli
 def test_read_overrides_from_file(tmpdir):
     os.chdir(tmpdir)
-    settings_dict = {"backup_dir": ".backups", "backup_depth": 6}
+    settings_dict = {"backup_depth": 6}
     with open(".config", "w") as fout:
         fout.write(yaml.safe_dump(settings_dict, default_flow_style=False))
     assert read_overrides_from_file(".config") == settings_dict
