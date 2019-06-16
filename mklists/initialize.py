e749b144e7be2ff1536e341a3dbee15196e7bc93
Tom Baker <tom@tombaker.org>
2019-06-16 13:22:12 +0200

    improving initialize.py

--- mklists/initialize.py
+++ mklists/initialize.py
@@ -20,18 +20,27 @@ files2dirs: {
 }
 """
 INITIAL_GLOBALRULE_YAMLFILE_STR = """\
-- [0, '.',          all_lines, lines,            0]
-- [0, '^=',         all_lines, move_to_a.txt,    1]
-- [0, '^2019|2020', all_lines, move_to_logs.txt, 1]
+# This file: Global rules, applied before rules specific to a list folder.
+# Put here any rules that apply to multiple list folders.
+- [0, '.',          x,         lines,            0]
+- [0, '^=',         lines,     move_to_a.txt,    1]
+- [0, '^2019|2020', lines,     move_to_logs.txt, 1]
+"""
+INITIAL_MINIMAL_RULEA_YAMLFILE_STR = """\
+- [0, '.',       lines,      consolidated_lines,   0]
 """
 INITIAL_EXAMPLE_RULEA_YAMLFILE_STR = """\
+# This file: Rules specific to this list folder.
+# At runtime, these rules are appended to the global rules.
 - [0, '.',       lines,      todo.txt,   0]
 - [0, '.',       to_a.txt    todo.txt,   1]
-- [1, 'NOW',     todo.txt,    now.txt,   1]
-- [1, 'LATER',   todo.txt,  later.txt,   0]
+- [1, 'NOW',     todo.txt,   now.txt,    1]
+- [1, 'LATER',   todo.txt,   later.txt,  0]
 """
 INITIAL_EXAMPLE_RULEB_YAMLFILE_STR = """\
-- [0, '.',       lines,         b.txt,   0]
+# This file: Rules specific to this list folder.
+# At runtime, these rules are appended to the global rules.
+- [0, '.',       lines,      b.txt,      0]
 - [1, '^2019',   b.txt,      2019.txt,   1]
 - [1, '^2020',   b.txt,      2020.txt,   0]
 """
