91aa049cd7531ffbe3a19670e40a84e386e64a76
Tom Baker <tom@tombaker.org>
2019-07-21 14:32:49 +0200

    rename get_datadir_pathnames_under_rootdir => somedir

--- tests/test_utils_get_datadir_pathnames_under_somedir_DONE.py
+++ tests/test_utils_get_datadir_pathnames_under_somedir_DONE.py
@@ -2,10 +2,10 @@
 
 import os
 from mklists.initialize import CONFIG_YAMLFILE_NAME, RULE_YAMLFILE_NAME
-from mklists.utils import get_datadir_pathnames_under_rootdir
+from mklists.utils import get_datadir_pathnames_under_somedir
 
 
-def test_get_datadir_pathnames_under_rootdir(tmpdir):
+def test_get_datadir_pathnames_under_somedir(tmpdir):
     """List data directories found under project root."""
     tmpdir.join(CONFIG_YAMLFILE_NAME).write("config stuff")
     tmpdir.join(RULE_YAMLFILE_NAME).write("some rules")
@@ -22,10 +22,10 @@ def test_get_datadir_pathnames_under_rootdir(tmpdir):
         os.path.join(tmpdir, "a/b"),
         os.path.join(tmpdir, "c"),
     ]
-    assert get_datadir_pathnames_under_rootdir(tmpdir) == expected
+    assert get_datadir_pathnames_under_somedir(tmpdir) == expected
 
 
-def test_get_datadir_pathnames_under_rootdir_excluding_hiddendirs(tmpdir):
+def test_get_datadir_pathnames_under_somedir_excluding_hiddendirs(tmpdir):
     """List data directories found under project root."""
     tmpdir.join(CONFIG_YAMLFILE_NAME).write("config stuff")
     tmpdir.join(RULE_YAMLFILE_NAME).write("some rules")
@@ -44,4 +44,4 @@ def test_get_datadir_pathnames_under_rootdir_excluding_hiddendirs(tmpdir):
         os.path.join(tmpdir, "a/b"),
         os.path.join(tmpdir, "c"),
     ]
-    assert get_datadir_pathnames_under_rootdir(tmpdir) == expected
+    assert get_datadir_pathnames_under_somedir(tmpdir) == expected
