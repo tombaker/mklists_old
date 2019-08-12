6173fe4e353dc3b3518ade5448bd03466b9c8487
Tom Baker <tom@tombaker.org>
2019-08-12 06:07:35 +0200

    consolidate pylintrc files - delete unneeded test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,14 +10,3 @@ def reinitialize_ruleclass_variables():
         for each test of Rule.sources_list"""
     Rule.sources_list = []
     Rule.sources_list_is_initialized = False
-
-
-@pytest.fixture(scope="module")
-def ruleobj_list():
-    """Returns list of Rule objects."""
-    return [
-        Rule(0, ".", "lines", "__RENAME__", 0),
-        Rule(0, "^= 20", "__RENAME__", "calendar", 1),
-        Rule(0, "NOW", "lines", "__RENAME__", 0),
-        Rule(0, "LATER", "__RENAME__", "calendar", 1),
-    ]
