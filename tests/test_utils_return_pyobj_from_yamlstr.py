bb01d98bb887fcd3bbd6feb75b4aebadf35973bd
Tom Baker <tom@tombaker.org>
2019-08-14 09:21:35 +0200

    down to five skipped tests, with 86 passing

--- tests/test_utils_return_pyobj_from_yamlstr.py
+++ tests/test_utils_return_pyobj_from_yamlstr.py
@@ -5,60 +5,33 @@ import pytest
 from mklists.utils import return_pyobj_from_yamlstr
 
 
-@pytest.mark.skip
-def test_return_pyobj_from_yamlstr(tmpdir):
-    """@@@Docstring"""
-    os.chdir(tmpdir)
-    yaml_str = "backups: 3\nverbose: false"
-    yamlfile = "mklists.yml"
-    with open(yamlfile, "w") as fout:
-        fout.write(yaml_str)
+def test_read_return_pyobj_from_yamlstr_given_good_yamlstr(tmpdir):
+    """Returns Python object given a YAML string."""
+    yamlstr = "backups: 3\nverbose: false"
     result = {"backups": 3, "verbose": False}
-    assert return_pyobj_from_yamlstr(yamlfile) == result
+    assert return_pyobj_from_yamlstr(_yamlstr=yamlstr) == result
 
 
-@pytest.mark.skip
-def test_return_pyobj_from_yamlstr_bad_yaml(tmpdir):
-    """Test with bad YAML."""
-    os.chdir(tmpdir)
-    yaml_str = "- backups: 4\n+ verbose: false"
-    yamlfile = "mklists.yml"
-    with open(yamlfile, "w") as fout:
-        fout.write(yaml_str)
-    with pytest.raises(SystemExit):
-        return_pyobj_from_yamlstr(yamlfile)
-
-
-@pytest.mark.skip
-def test_return_pyobj_from_yamlstr_file_not_found(tmpdir):
-    """Test with bad YAML."""
-    os.chdir(tmpdir)
-    yamlfile = "mklists.yml"
-    with pytest.raises(SystemExit):
-        return_pyobj_from_yamlstr(yamlfile)
-
-
-@pytest.mark.skip
-def test_read_yaml_config_yamlfile_given_good_yamlfile(tmpdir):
-    """Writes string to YAML rulefile, reads back to list of lists."""
-    os.chdir(tmpdir)
-    lrules_yamlfile_str = """
+def test_read_return_pyobj_from_yamlstr_given_another_good_yamlstr(tmpdir):
+    """Returns Python object given a YAML string."""
+    yamlstr = """
     - [1, 'NOW', a, b, 0]
     - [1, 'LATER', a, c, 0]"""
-    with open("_lrules", "w") as fout:
-        fout.write(lrules_yamlfile_str)
-    good_pyobject = [[1, "NOW", "a", "b", 0], [1, "LATER", "a", "c", 0]]
-    assert return_pyobj_from_yamlstr("_lrules") == good_pyobject
+    result = [[1, "NOW", "a", "b", 0], [1, "LATER", "a", "c", 0]]
+    assert return_pyobj_from_yamlstr(_yamlstr=yamlstr) == result
 
 
-@pytest.mark.skip
-def test_read_yaml_config_yamlfile_given_bad_yaml(tmpdir):
-    """Trying to write bad string to YAML rulefile raises SystemExit."""
-    os.chdir(tmpdir)
+def test_return_pyobj_from_yamlstr_given_bad_yaml(tmpdir):
+    """Raises SystemExit when given bad YAML."""
+    bad_yamlstr = "- backups: 4\n+ verbose: false"
+    with pytest.raises(SystemExit):
+        return_pyobj_from_yamlstr(_yamlstr=bad_yamlstr)
+
+
+def test_read_yaml_config_yamlfile_given_more_bad_yaml(tmpdir):
+    """Raises SystemExit when given bad YAML."""
     bad_yamlstr = """
     - [1, 'NOW', a, b, 0]
     + [1, 'LATER', a, c, 0]"""
-    with open("_lrules_bad", "w") as fout:
-        fout.write(bad_yamlstr)
     with pytest.raises(SystemExit):
-        return_pyobj_from_yamlstr("_lrules_bad")
+        return_pyobj_from_yamlstr(_yamlstr=bad_yamlstr)
--- tests/test_utils_return_yamlstr_from_yamlfile.py
+++ tests/test_utils_return_yamlstr_from_yamlfile.py
@@ -9,19 +9,27 @@ from mklists.utils import return_yamlstr_from_yamlfile
 
 
 def test_return_yamlstr_from_yamlfile(tmpdir):
-    """Writes YAML string to YAML file,
-    then reads file back to a YAML string."""
+    """Writes YAML string to YAML file, then reads file back to a YAML string."""
     os.chdir(tmpdir)
-    yaml_str = "backups: 3\nverbose: false"
+    yamlstr = "backups: 3\nverbose: false"
     yamlfile_name = "mklists.yml"
     with open(yamlfile_name, "w") as fout:
-        fout.write(yaml_str)
-    assert io.open(yamlfile_name).read() == yaml_str
-    assert return_yamlstr_from_yamlfile(_yamlfile_name=yamlfile_name) == yaml_str
+        fout.write(yamlstr)
+    assert io.open(yamlfile_name).read() == yamlstr
+    assert return_yamlstr_from_yamlfile(_yamlfile_name=yamlfile_name) == yamlstr
 
 
 def test_return_yamlstr_from_yamlfile_notfound(tmpdir):
-    """Tries to read a non-existent YAML file and exits with error message."""
+    """Tries to read a non-existent YAML file: SystemExit with error message"""
     os.chdir(tmpdir)
+    yamlfile_name = "mklists.yml"
+    with pytest.raises(SystemExit):
+        return_yamlstr_from_yamlfile(_yamlfile_name=yamlfile_name)
+
+
+def test_return_yamlstr_from_yamlfile_notfound_extra(tmpdir):
+    """Tries to read a non-existent YAML file: YamlFileNotFoundError with error message"""
+    os.chdir(tmpdir)
+    yamlfile_name = "mklists.yml"
     with pytest.raises(YamlFileNotFoundError):
-        return_yamlstr_from_yamlfile(_yamlfile_name=CONFIG_YAMLFILE_NAME)
+        return_yamlstr_from_yamlfile(_yamlfile_name=yamlfile_name)
