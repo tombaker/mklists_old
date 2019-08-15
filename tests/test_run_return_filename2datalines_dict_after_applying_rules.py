516573a5fbfe652971b4c46ba3c7f86668336cb7
Tom Baker <tom@tombaker.org>
2019-08-15 12:17:28 +0200

    improve clarity of docstrings and tests for function applying rules to datalines

---
+++ tests/test_run_return_filename2datalines_dict_after_applying_rules.py
@@ -0,0 +1,94 @@
+"""Returns dictionary (keys are filenames, values are lists of text lines),
+given list of rule objects and list of text lines aggregated from data files."""
+
+import pytest
+from mklists.run import return_filename2datalines_dict_after_applying_rules_to_lines
+from mklists.rules import Rule
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_correct_result():
+    """Returns correct dictionary from good inputs."""
+    ruleobjs_list = [Rule(0, "i", "a.txt", "b.txt", 0)]
+    datalines_list = ["two ticks\n", "an ant\n", "the mite\n"]
+    result_dict = {"a.txt": ["an ant\n"], "b.txt": ["two ticks\n", "the mite\n"]}
+    return_filename2datalines_dict_after_applying_rules_to_lines(
+        _ruleobjs_list=ruleobjs_list, _datalines_list=datalines_list
+    ) == result_dict
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_another_correct_result():
+    """Returns correct dictionary from good inputs."""
+    ruleobjs_list = [Rule(2, "i", "a.txt", "b.txt", 1)]
+    datalines_list = ["two ticks\n", "an ant\n", "the mite\n"]
+    result_dict = {"a.txt": ["an ant\n"], "b.txt": ["the mite\n", "two ticks\n"]}
+    return_filename2datalines_dict_after_applying_rules_to_lines(
+        _ruleobjs_list=ruleobjs_list, _datalines_list=datalines_list
+    ) == result_dict
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_yet_another_correct_result():
+    """Returns correct dictionary from good inputs."""
+    ruleobjs_list = [
+        Rule(1, "NOW", "a.txt", "now.txt", 0),
+        Rule(1, "LATER", "a.txt", "later.txt", 0),
+    ]
+    datalines_list = ["NOW Summer\n", "LATER Winter\n"]
+    result_dict = {
+        "now.txt": ["NOW Summer\n"],
+        "later.txt": ["LATER Winter\n"],
+        "a.txt": [],
+    }
+    return_filename2datalines_dict_after_applying_rules_to_lines(
+        _ruleobjs_list=ruleobjs_list, _datalines_list=datalines_list
+    ) == result_dict
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_correct_result_too():
+    """Returns correct dictionary from good inputs."""
+    ruleobjs_list = [Rule(1, ".", "a.txt", "now.txt", 1)]
+    datalines_list = ["LATER Winter\n", "NOW Summer\n"]
+    result_dict = {"now.txt": ["NOW Summer\n", "LATER Winter\n"], "a.txt": []}
+    return_filename2datalines_dict_after_applying_rules_to_lines(
+        _ruleobjs_list=ruleobjs_list, _datalines_list=datalines_list
+    ) == result_dict
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_no_rules_specified():
+    """Exits with error if list of rule objects is not passed as an argument."""
+    datalines_list = [["a line\n"]]
+    with pytest.raises(SystemExit):
+        return_filename2datalines_dict_after_applying_rules_to_lines(
+            _ruleobjs_list=None, _datalines_list=datalines_list
+        )
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_no_rules_specified_either():
+    """Exits with error if rule objects list passed as argument is empty."""
+    ruleobjs_list = []
+    datalines_list = ["NOW Summer\n", "LATER Winter\n"]
+    with pytest.raises(SystemExit):
+        return_filename2datalines_dict_after_applying_rules_to_lines(
+            _ruleobjs_list=ruleobjs_list, _datalines_list=datalines_list
+        )
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_no_data_specified():
+    """Exits with error no datalines list is passed as argument."""
+    ruleobjs_list = [[Rule(1, "a", "b", "c", 2)]]
+    with pytest.raises(SystemExit):
+        return_filename2datalines_dict_after_applying_rules_to_lines(
+            _ruleobjs_list=ruleobjs_list, _datalines_list=None
+        )
+
+
+def test_return_filename2datalines_dict_after_applying_rules_to_lines_no_data_specified_either():
+    """Exits with error if datalines list passed as argument is empty."""
+    ruleobjs_list = [
+        Rule(1, "NOW", "a.txt", "now.txt", 0),
+        Rule(1, "LATER", "a.txt", "later.txt", 0),
+    ]
+    datalines_list = []
+    with pytest.raises(SystemExit):
+        return_filename2datalines_dict_after_applying_rules_to_lines(
+            _ruleobjs_list=ruleobjs_list, _datalines_list=datalines_list
+        )
