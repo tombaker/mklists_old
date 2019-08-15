c52d952f97548dae0bbe356e596a369a906024c4
Tom Baker <tom@tombaker.org>
2019-08-15 22:06:05 +0200

    test_booleans_is_match_to_rule_as_line.py

--- tests/test_booleans_is_match_to_rule_as_line.py
+++ tests/test_booleans_is_match_to_rule_as_line.py
@@ -1,38 +1,35 @@
-"""Tests for run.py
-
-Note that function seems to work regardless of whether values
-are passed as positional or keyword arguments.
-"""
+"""Returns True if line (or part of line) matches a given regular expression."""
 
 from mklists.booleans import is_match_to_rule_as_line
 from mklists.rules import Rule
 
 
-def test_apply_is_match_to_rule_as_line():
-    """@@@Docstring"""
+def test_is_match_to_rule_as_line():
+    """Returns True: matches simple regex in field 1."""
     given_ruleobj = Rule(1, "NOW", "a.txt", "b.txt", 0)
-    is_match_to_rule_as_line(given_ruleobj, "NOW Buy milk") is True
+    assert is_match_to_rule_as_line(given_ruleobj, "NOW Buy milk")
 
 
-def test_apply_is_match_to_rule_as_line_with_space():
-    """@@@Docstring"""
+def test_is_match_to_rule_as_line_with_space():
+    """Returns True: matches regex in field 1, despite leading whitespace."""
     given_ruleobj = Rule(1, "NOW", "a.txt", "b.txt", 0)
-    is_match_to_rule_as_line(given_ruleobj, " NOW Buy milk") is True
+    assert is_match_to_rule_as_line(given_ruleobj, " NOW Buy milk")
 
 
-def test_apply_is_match_to_rule_as_line_no_match():
-    """@@@Docstring"""
+def test_is_match_to_rule_as_line_no_match():
+    """Returns False: does not match simple regex in field 1."""
     given_ruleobj = Rule(1, "NOW", "a.txt", "b.txt", 0)
-    is_match_to_rule_as_line(given_ruleobj, "LATER Buy milk") is False
+    assert not is_match_to_rule_as_line(given_ruleobj, "LATER Buy milk")
 
 
-def test_apply_is_match_to_rule_as_line_gotcha():
-    """True because ' NOW Buy milk'.split() => ['NOW', 'Buy', 'milk']"""
+def test_is_match_to_rule_as_line_gotcha():
+    """Returns True despite the leading whitespace in the line
+    because ' NOW Buy milk'.split() => ['NOW', 'Buy', 'milk']."""
     given_ruleobj = Rule(1, "^NOW", "a.txt", "b.txt", 0)
-    is_match_to_rule_as_line(given_ruleobj, " NOW Buy milk") is True
+    assert is_match_to_rule_as_line(given_ruleobj, " NOW Buy milk")
 
 
-def test_apply_is_match_to_rule_as_line_entire_line():
-    """@@@Docstring"""
+def test_is_match_to_rule_as_line_entire_line():
+    """Returns True because regex matches the start of the entire line."""
     given_ruleobj = Rule(0, "^NOW", "a.txt", "b.txt", 0)
-    is_match_to_rule_as_line(given_ruleobj, "NOW Buy milk") is True
+    assert is_match_to_rule_as_line(given_ruleobj, "NOW Buy milk")
