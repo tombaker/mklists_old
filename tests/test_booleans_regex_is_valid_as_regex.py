a93211bbda98ae77cf50ad3a670ef1889a695f1e
Tom Baker <tom@tombaker.org>
2019-09-28 12:33:32 +0900

    add another test for regex_is_valid_as_regex

--- tests/test_booleans_regex_is_valid_as_regex.py
+++ tests/test_booleans_regex_is_valid_as_regex.py
@@ -7,3 +7,9 @@ from mklists.booleans import regex_is_valid_as_regex
 def test_regex_is_valid_as_regex():
     """Returns True because regex is valid."""
     assert regex_is_valid_as_regex("$NOW")
+
+
+def test_regex_is_valid_as_regex_unterminated_character_set():
+    """Returns False because regex is invalid
+    because character set is not terminated."""
+    assert not regex_is_valid_as_regex("[")
