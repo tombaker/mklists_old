c2ce2d81539ab9889a84c264c7f0ee819bbca7fb
Tom Baker <tom@tombaker.org>
2019-09-29 08:56:56 +0200

    test_move_specified_datafiles_elsewhere_as_external_dir passes

--- tests/test_run_move_specified_datafiles_elsewhere.py
+++ tests/test_run_move_specified_datafiles_elsewhere.py
@@ -51,3 +51,28 @@ def test_move_specified_datafiles_elsewhere_as_subdir(tmpdir):
     )
     os.chdir(tmpdir_agendabdir)
     assert "agendab.txt" in os.listdir(tmpdir_agendabdir)
+
+
+@pytest.mark.now
+def test_move_specified_datafiles_elsewhere_as_external_dir(tmpdir):
+    """Test of move_specified_datafiles_elsewhere where:
+    * file moved to directory outside of the repo
+    * assumes existence of a /tmp directory in filesystem
+
+    Destination directory defined relative to root of filesystem"""
+    tmpdir_listsdir = tmpdir.mkdir("lists")
+    tmpdir_agendaadir = tmpdir_listsdir.mkdir("agendaa")
+    tmpdir_agendaadir.join("agendab.txt").write("some content")
+    tmpdir_agendabdir = tmpdir_listsdir.mkdir("agendab")
+    tmpdir_agendabdir.join("agendaa.txt").write("some content")
+    filenames2dirnames_dict = {"agendaa.txt": "lists/agendaa", "agendab.txt": "/tmp"}
+    rootdir_pathname = tmpdir
+    if os.path.exists("/tmp/agendab.txt"):  # clean-up, sometimes needed
+        os.remove("/tmp/agendab.txt")
+    os.chdir(tmpdir_agendaadir)
+    move_specified_datafiles_elsewhere(
+        _filenames2dirnames_dict=filenames2dirnames_dict,
+        _rootdir_pathname=rootdir_pathname,
+    )
+    os.chdir("/tmp")
+    assert "agendab.txt" in os.listdir("/tmp")
