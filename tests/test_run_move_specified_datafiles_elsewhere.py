553809773ce86fb626f041704df26ddee2950508
Tom Baker <tom@tombaker.org>
2019-09-29 08:45:35 +0200

    test_move_specified_datafiles_elsewhere_as_subdir passes

--- tests/test_run_move_specified_datafiles_elsewhere.py
+++ tests/test_run_move_specified_datafiles_elsewhere.py
@@ -8,7 +8,9 @@ from mklists.run import move_specified_datafiles_elsewhere
 
 @pytest.mark.now
 def test_move_specified_datafiles_elsewhere(tmpdir):
-    """@@@Docstring"""
+    """Basic test of move_specified_datafiles_elsewhere plus:
+    * extra test of filenames2dirnames_dict
+    * extra test of file system before move_specified_datafiles_elsewhere run"""
     tmpdir_agendaadir = tmpdir.mkdir("agendaa")
     tmpdir_agendaadir.join("agendab.txt").write("some content")
     tmpdir_agendabdir = tmpdir.mkdir("agendab")
@@ -17,6 +19,31 @@ def test_move_specified_datafiles_elsewhere(tmpdir):
     rootdir_pathname = tmpdir
     assert filenames2dirnames_dict["agendaa.txt"] == "agendaa"
     assert "agendab.txt" in os.listdir(tmpdir_agendaadir)
+    os.chdir(tmpdir_agendaadir)
+    move_specified_datafiles_elsewhere(
+        _filenames2dirnames_dict=filenames2dirnames_dict,
+        _rootdir_pathname=rootdir_pathname,
+    )
+    os.chdir(tmpdir_agendabdir)
+    assert "agendab.txt" in os.listdir(tmpdir_agendabdir)
+
+
+@pytest.mark.now
+def test_move_specified_datafiles_elsewhere_as_subdir(tmpdir):
+    """Test of move_specified_datafiles_elsewhere where:
+    * file moved to specific subdirectory under repo root directory
+
+    Note that destination directory is defined relative to repo root directory"""
+    tmpdir_listsdir = tmpdir.mkdir("lists")
+    tmpdir_agendaadir = tmpdir_listsdir.mkdir("agendaa")
+    tmpdir_agendaadir.join("agendab.txt").write("some content")
+    tmpdir_agendabdir = tmpdir_listsdir.mkdir("agendab")
+    tmpdir_agendabdir.join("agendaa.txt").write("some content")
+    filenames2dirnames_dict = {
+        "agendaa.txt": "lists/agendaa",
+        "agendab.txt": "lists/agendab",
+    }
+    rootdir_pathname = tmpdir
     os.chdir(tmpdir_agendaadir)
     move_specified_datafiles_elsewhere(
         _filenames2dirnames_dict=filenames2dirnames_dict,
