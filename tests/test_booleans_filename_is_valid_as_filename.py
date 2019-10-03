8f4eb4944174316b1022cc6b1cc249a85c6d5c46
Tom Baker <tom@tombaker.org>
2019-10-03 18:01:10 +0200

    109 passed, 25 skipped

--- tests/test_booleans_filename_is_valid_as_filename.py
+++ tests/test_booleans_filename_is_valid_as_filename.py
@@ -5,13 +5,11 @@ import pytest
 from mklists.booleans import filename_is_valid_as_filename
 
 
-@pytest.mark.skip
 def test_utils_filename_is_valid_as_filename_exits_filename_uses_illegal_character():
     """Semicolon is illegal in filename."""
-    assert filename_is_valid_as_filename(_filename="foo;bar.txt") is False
+    assert filename_is_valid_as_filename(filename="foo;bar.txt") is False
 
 
-@pytest.mark.skip
 def test_utils_filename_is_valid_as_filename_exits_already_used_as_directory_name(
     tmpdir
 ):
@@ -20,10 +18,9 @@ def test_utils_filename_is_valid_as_filename_exits_already_used_as_directory_nam
     tmpdir.mkdir("foobar")
     os.chdir(tmpdir)
     with pytest.raises(SystemExit):
-        filename_is_valid_as_filename(_filename=fname)
+        filename_is_valid_as_filename(filename=fname)
 
 
-@pytest.mark.skip
 def test_utils_filename_is_valid_as_filename():
     r"""Note: previously assigned bad patterns as follows:
     bad_patterns = [r"\.swp$", r"\.tmp$", r"~$", r"^\."]
@@ -35,44 +32,41 @@ def test_utils_filename_is_valid_as_filename():
     fname = "foobar.txt"
     bad_patterns = ["\\.swp$", "\\.tmp$", "~$", "^\\."]
     assert filename_is_valid_as_filename(
-        _filename=fname, _invalid_filename_regexes_list=bad_patterns
+        filename=fname, invalid_filename_regexes_list=bad_patterns
     )
 
 
-@pytest.mark.skip
 def test_utils_filename_is_valid_as_filename_dotfile():
     """@@@Docstring"""
     fname = ".foobar.txt"
     bad_patterns = ["\\.swp$", "\\.tmp$", "~$", "^\\."]
     assert (
         filename_is_valid_as_filename(
-            _filename=fname, _invalid_filename_regexes_list=bad_patterns
+            filename=fname, invalid_filename_regexes_list=bad_patterns
         )
         is False
     )
 
 
-@pytest.mark.skip
 def test_utils_filename_is_valid_as_filename_dotfile_emacs_backup_file():
     """@@@Docstring"""
     fname = "foobar.txt~"
     bad_patterns = ["\\.swp$", "\\.tmp$", "~$", "^\\."]
     assert (
         filename_is_valid_as_filename(
-            _filename=fname, _invalid_filename_regexes_list=bad_patterns
+            filename=fname, invalid_filename_regexes_list=bad_patterns
         )
         is False
     )
 
 
-@pytest.mark.skip
 def test_utils_filename_is_valid_as_filename_bad_filename_extension():
     """@@@Docstring"""
     fname = "foobar.swp"
     bad_patterns = ["\\.swp$", "\\.tmp$", "~$", "^\\."]
     assert (
         filename_is_valid_as_filename(
-            _filename=fname, _invalid_filename_regexes_list=bad_patterns
+            filename=fname, invalid_filename_regexes_list=bad_patterns
         )
         is False
     )
