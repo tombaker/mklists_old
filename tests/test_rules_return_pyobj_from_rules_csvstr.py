030631bb0b67f7b5a9ab981ffaf1f2f3df0edd58
Tom Baker <tom@tombaker.org>
2020-01-13 15:07:50 +0100

    working through failing tests...

--- tests/test_rules_return_pyobj_from_rules_csvstr.py
+++ tests/test_rules_return_pyobj_from_rules_csvstr.py
@@ -6,66 +6,29 @@ import pytest
 
 # from mklists.constants import RULE_CSVFILE_NAME
 from mklists.exceptions import NoRulesError
-from mklists.rules import Rule, _return_pyobj_from_rules_csvstr
+from mklists.rules import _return_pyobj_from_rules_csvstr
 
 # pylint: disable=unused-argument
 # In tests, fixture arguments may look like they are unused.
 
-TEST_RULES_YAMLSTR = r"""# Test rules for this module only.
-- [0, '.',          x,         lines,            0]
-- [1, 'NOW',        lines,     alines,           1]
-- [1, 'LATER',      lines,     alines,           1]
-- [0, '^2019|^2020', lines,     blines,           1]"""
+TEST_RULES_CSVSTR = "0|.|x|lines|0|A comment\n1|NOW|lines|alines|1|Another comment\n1|LATER|lines|alines|1|\n0|^2019-...|lines|blines|1|\n"
 
-TEST_RULES_YAMLSTR_PARSED = [
-    [0, ".", "x", "lines", 0],
-    [1, "NOW", "lines", "alines", 1],
-    [1, "LATER", "lines", "alines", 1],
-    [0, "^2019|^2020", "lines", "blines", 1],
-]
-
-TEST_RULEOBJ_LIST = [
-    Rule(
-        source_matchfield=0,
-        source_matchpattern=".",
-        source="x",
-        target="lines",
-        target_sortorder=0,
-    ),
-    Rule(
-        source_matchfield=1,
-        source_matchpattern="NOW",
-        source="lines",
-        target="alines",
-        target_sortorder=1,
-    ),
-    Rule(
-        source_matchfield=1,
-        source_matchpattern="LATER",
-        source="lines",
-        target="alines",
-        target_sortorder=1,
-    ),
-    Rule(
-        source_matchfield=0,
-        source_matchpattern="^2019|^2020",
-        source="lines",
-        target="blines",
-        target_sortorder=1,
-    ),
+PYOBJ = [
+    ["0", ".", "x", "lines", "0", "A comment"],
+    ["1", "NOW", "lines", "alines", "1", "Another comment"],
+    ["1", "LATER", "lines", "alines", "1", ""],
+    ["0", "^2019 ..", "lines", "blines", "1", ""],
 ]
 
 
-def test_run_return_ruleobj_list_from_pyobj(tmpdir):
+def test_run_return_pyobj_from_rules_csvstr(tmpdir):
     """@@@Docstring"""
-    expected = TEST_RULEOBJ_LIST
-    real = _return_pyobj_from_rules_csvstr(csvstr=TEST_RULES_YAMLSTR)
+    expected = PYOBJ
+    real = _return_pyobj_from_rules_csvstr(csvstr=TEST_RULES_CSVSTR)
     assert real == expected
 
 
-def test_run_return_ruleobj_list_from_pyobj_no_rules(tmpdir):
+def test_run_return_pyobj_from_rules_csvstr_no_rules(tmpdir):
     """@@@Docstring"""
-    # 2019-09-30: Problem has something to do with testing for
-    # wrong exception raised
     with pytest.raises(NoRulesError):
         _return_pyobj_from_rules_csvstr(csvstr=None)
