87293b2f0f86c9d4450a6296bf3c1232b8143675
Tom Baker <tom@tombaker.org>
2020-01-26 14:14:40 +0100

    tmpdir => tmp_path in return_datadir_pathnames... - all 4 tests passing

--- tests/utils/test_return_datadir_pathnames_under_given_pathname.py
+++ tests/utils/test_return_datadir_pathnames_under_given_pathname.py
@@ -1,14 +1,13 @@
 """Return list of directories with .rules files under (but not including) root."""
 
 import os
-import pytest
 from pathlib import Path
 from mklists.utils import return_datadir_pathnames_under_given_pathname
 from mklists.constants import CONFIG_YAMLFILE_NAME, RULES_CSVFILE_NAME
 
 
 def test_return_datadir_pathnames_under_given_pathname_excluding_rootdir(tmp_path):
-    """List directories with rule files under (but not including) root directory."""
+    """List data directories (ie, with rulefiles) under (but not including) root."""
     os.chdir(tmp_path)
     abc = Path.cwd().joinpath("a/b/c")
     abc.mkdir(parents=True, exist_ok=True)
@@ -21,47 +20,46 @@ def test_return_datadir_pathnames_under_given_pathname_excluding_rootdir(tmp_pat
     assert return_datadir_pathnames_under_given_pathname() == expected
 
 
-@pytest.mark.skip
-def test_return_datadir_pathnames_under_given_pathname_excluding_hiddendirs(tmpdir):
-    """List rulefile pathnames found under project root."""
-    tmpdir.join(CONFIG_YAMLFILE_NAME).write("config stuff")
-    tmpdir.join(RULES_CSVFILE_NAME).write("some rules")
-    tmpdir_hidden = tmpdir.mkdir(".hidden")
-    tmpdir_hidden.join(RULES_CSVFILE_NAME).write("some rules")
-    tmpdira = tmpdir.mkdir("a")
-    tmpdira.join(RULES_CSVFILE_NAME).write("some rules")
-    tmpdirb = tmpdira.mkdir("b")
-    tmpdirb.join(RULES_CSVFILE_NAME).write("some rules")
-    tmpdirc = tmpdir.mkdir("c")
-    tmpdirc.join(RULES_CSVFILE_NAME).write("some rules")
-    os.chdir(tmpdir)
-    expected = [
-        os.path.join(tmpdir, "a"),
-        os.path.join(tmpdir, "a/b"),
-        os.path.join(tmpdir, "c"),
-    ]
+def test_return_datadir_pathnames_under_given_pathname_excluding_hiddendirs(tmp_path):
+    """List data directories ignoring a "hidden" directory."""
+    os.chdir(tmp_path)
+    ab = Path.cwd().joinpath("a/b")
+    ab.mkdir(parents=True, exist_ok=True)
+    c = Path.cwd().joinpath("c")
+    c.mkdir()
+    hidden = Path.cwd().joinpath(".hidden")
+    hidden.mkdir()
+    Path(CONFIG_YAMLFILE_NAME).write_text("config stuff")
+    Path(RULES_CSVFILE_NAME).write_text("rule stuff")
+    Path(tmp_path).joinpath("a", RULES_CSVFILE_NAME).write_text("rule_stuff")
+    Path(tmp_path).joinpath("a/b", RULES_CSVFILE_NAME).write_text("rule_stuff")
+    Path(tmp_path).joinpath("c", RULES_CSVFILE_NAME).write_text("rule_stuff")
+    Path(tmp_path).joinpath(".hidden", RULES_CSVFILE_NAME).write_text("rule_stuff")
+    expected = [Path(tmp_path) / "a", Path(tmp_path) / "a/b", Path(tmp_path) / "c"]
     assert return_datadir_pathnames_under_given_pathname() == expected
 
 
-@pytest.mark.skip
-def test_return_datadir_pathnames_under_given_pathname_just_one(tmpdir):
-    """List rulefile pathnames found under project root."""
-    tmpdir.join(CONFIG_YAMLFILE_NAME).write("config stuff")
-    tmpdir.join(RULES_CSVFILE_NAME).write("some rules")
-    tmpdira = tmpdir.mkdir("a")
-    tmpdira.join(RULES_CSVFILE_NAME).write("some rules")
-    os.chdir(tmpdir)
-    expected = [os.path.join(tmpdir, "a")]
+def test_return_datadir_pathnames_under_given_pathname_just_one(tmp_path):
+    """List data directories when there is just one."""
+    os.chdir(tmp_path)
+    a = Path("a")
+    a.mkdir()
+    Path(CONFIG_YAMLFILE_NAME).write_text("config stuff")
+    Path(RULES_CSVFILE_NAME).write_text("rule stuff")
+    Path(tmp_path).joinpath("a", RULES_CSVFILE_NAME).write_text("rule_stuff")
+    expected = [Path(tmp_path).joinpath("a")]
     assert return_datadir_pathnames_under_given_pathname() == expected
 
 
-@pytest.mark.skip
-def test_return_datadir_pathnames_under_given_pathname_rootdir_has_no_rulefile(tmpdir):
-    """List rulefile pathnames found under project root where
-    there is no rulefile in the root directory itself."""
-    tmpdir.join(CONFIG_YAMLFILE_NAME).write("config stuff")
-    tmpdira = tmpdir.mkdir("a")
-    tmpdira.join(RULES_CSVFILE_NAME).write("some rules")
-    os.chdir(tmpdir)
-    expected = [os.path.join(tmpdir, "a")]
+def test_return_datadir_pathnames_under_given_pathname_rootdir_has_no_rulefile(
+    tmp_path
+):
+    """List data directories even when root directory has no rulefile."""
+    os.chdir(tmp_path)
+    a = Path("a")
+    a.mkdir()
+    Path(CONFIG_YAMLFILE_NAME).write_text("config stuff")
+    # NOT Path(RULES_CSVFILE_NAME).write_text("rule stuff")
+    Path(tmp_path).joinpath("a", RULES_CSVFILE_NAME).write_text("rule_stuff")
+    expected = [Path(tmp_path).joinpath("a")]
     assert return_datadir_pathnames_under_given_pathname() == expected
