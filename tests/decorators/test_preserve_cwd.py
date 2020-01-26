d9694fcda60e480575327e0671e0c1b45bfb3e75
Tom Baker <tom@tombaker.org>
2020-01-26 11:06:24 +0100

    tmpdir => tmp_path in test_preserve_cwd

--- tests/decorators/test_preserve_cwd.py
+++ tests/decorators/test_preserve_cwd.py
@@ -1,25 +1,24 @@
-"""Tests for decorator @preserve_cwd
-
-Edit /Users/tbaker/github/tombaker/mklists/mklists/decorators.py
-"""
+"""Tests for decorator @preserve_cwd"""
 
 import os
+from pathlib import Path
 from mklists.decorators import preserve_cwd
 
 
-def test_preserve_cwd(tmpdir):
+def test_preserve_cwd(tmp_path):
     """@@@Docstring"""
-    os.chdir(tmpdir)
-    directory_before = os.getcwd()
-    tmpdir_some_directory = tmpdir.mkdir("some_directory")
+    os.chdir(tmp_path)
+    directory_before = Path.cwd()
+    tmpdir_some_directory = Path(tmp_path) / "some_directory"
+    Path(tmpdir_some_directory).mkdir()
 
     @preserve_cwd
     def change_directory_to(change_to=None):
         os.chdir(change_to)
-        return os.getcwd()
+        return Path.cwd()
 
     directory_returned = change_directory_to(tmpdir_some_directory)
-    directory_after = os.getcwd()
+    directory_after = Path.cwd()
 
     assert tmpdir_some_directory == directory_returned
     assert directory_after == directory_before
