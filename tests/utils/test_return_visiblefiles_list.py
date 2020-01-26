cd4208c50db459e06785dfd64d6faef9a526955a
Tom Baker <tom@tombaker.org>
2020-01-26 12:56:30 +0100

    tmpdir => tmp_path in return_visiblefiles...

--- tests/utils/test_return_visiblefiles_list.py
+++ tests/utils/test_return_visiblefiles_list.py
@@ -1,22 +1,23 @@
 """@@@Docstring"""
 
 import os
+from pathlib import Path
 from mklists.utils import return_visiblefiles_list
 
 
-def test_return_visiblefiles_list(tmpdir):
+def test_return_visiblefiles_list(tmp_path):
     """Ignores directory 'baz'."""
-    os.chdir(tmpdir)
-    tmpdir.join("bar").write("bar stuff")
-    tmpdir.join("foo").write("foo stuff")
-    tmpdir.mkdir("baz")
+    os.chdir(tmp_path)
+    Path("bar").write_text("bar stuff")
+    Path("foo").write_text("foo stuff")
+    Path("baz").mkdir()
     assert return_visiblefiles_list() == ["bar", "foo"]
-    assert os.getcwd() == str(tmpdir)
+    assert Path.cwd() == Path(tmp_path)
 
 
-def test_return_visiblefiles_list_does_not_show_directories(tmpdir):
+def test_return_visiblefiles_list_does_not_show_directories(tmp_path):
     """Ignores dot file '.bar'."""
-    os.chdir(tmpdir)
-    tmpdir.join(".bar").write("bar stuff")
-    tmpdir.join("foo").write("foo stuff")
+    os.chdir(tmp_path)
+    Path(".bar").write_text("bar stuff")
+    Path("foo").write_text("foo stuff")
     assert return_visiblefiles_list() == ["foo"]
