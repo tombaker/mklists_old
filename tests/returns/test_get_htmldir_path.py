bed9d0bd5ead11d871cfaac80f0764942364f79c
Tom Baker <tom@tombaker.org>
2020-02-08 11:43:01 +0100

    finish get_htmldir_path - 130 passing - none skipped!

--- tests/returns/test_get_htmldir_path.py
+++ tests/returns/test_get_htmldir_path.py
@@ -11,32 +11,29 @@ HTMLDIR_NAME = "_html"
 def test_utils_return_htmldir_pathname():
     """Returns pathname of HTML directory."""
     rd = "/Users/tbaker/tmp"
-    hd = "_html"
+    hd = HTMLDIR_NAME
     dd = "/Users/tbaker/tmp/agenda"
-    expected = Path("/Users/tbaker/tmp/_html/agenda")
+    expected = Path("/Users/tbaker/tmp").joinpath(HTMLDIR_NAME, "agenda")
     assert get_htmldir_path(rootdir=rd, htmldir=hd, datadir=dd) == expected
 
 
 def test_utils_return_htmldir_pathname_html_subdirectories_nested():
     """Structure of HTML directory mirrors structure of data directories."""
     rd = "/Users/tbaker/tmp"
-    hd = "_html"
     dd = "/Users/tbaker/tmp/a/b"
-    expected = Path("/Users/tbaker/tmp/_html/a/b")
-    real = get_htmldir_path(rootdir=rd, htmldir=hd, datadir=dd)
+    expected = Path("/Users/tbaker/tmp").joinpath(HTMLDIR_NAME, "a/b")
+    real = get_htmldir_path(rootdir=rd, datadir=dd)
     assert real == expected
 
 
-@pytest.mark.skip
-def test_utils_return_htmldir_pathname_rootdir_pathname_not_given():
+def test_utils_return_htmldir_pathname_rootdir_pathname_not_given(tmp_path):
     """Raises exception if argument root not provided."""
-    hd = "_html"
+    os.chdir(tmp_path)
     dd = "agenda"
     with pytest.raises(SystemExit):
-        get_htmldir_path(rootdir=None, htmldir=hd, datadir=dd)
+        get_htmldir_path(rootdir=None, datadir=dd)
 
 
-@pytest.mark.skip
 def test_utils_return_htmldir_pathname_htmldir_name_not_given(tmp_path):
     """Raises exception if HTML directory name is deliberately omitted."""
     os.chdir(tmp_path)
@@ -44,14 +41,12 @@ def test_utils_return_htmldir_pathname_htmldir_name_not_given(tmp_path):
         get_htmldir_path(htmldir=None)
 
 
-@pytest.mark.skip
 def test_utils_return_htmldir_pathname_datadir_name_not_given(tmp_path):
     """Returns pathname of HTML directory."""
-    os.chdir(tmp_path)
-    Path("mklists.yml").write_text("config stuff")
-    # pylint: disable=unused-variable
-    # Yes, by design.
-    dirname, datadir = os.path.split(os.getcwd())
-    os.chdir(tmp_path)
-    expected = Path(tmp_path).joinpath(datadir)
-    assert get_htmldir_path(datadir=datadir) == expected
+    Path(tmp_path).joinpath("mklists.yml").write_text("config stuff")
+    ab = Path(tmp_path).joinpath("a/b")
+    ab.mkdir(parents=True, exist_ok=True)
+    os.chdir(ab)
+    assert HTMLDIR_NAME == "_html"
+    expected = Path(tmp_path) / HTMLDIR_NAME / "a/b"
+    assert get_htmldir_path() == expected
